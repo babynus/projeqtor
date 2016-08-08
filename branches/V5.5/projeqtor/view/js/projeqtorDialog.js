4126945f32c6a59465aa8ab76c36f5a34dfffb38
babynus <pascal.bernard@projeqtor.net>
2016-08-08 14:54:19 +0000

    V5.5.0

--- branches/V5.5/projeqtor/view/js/projeqtorDialog.js
+++ branches/V5.5/projeqtor/view/js/projeqtorDialog.js
@@ -5495,9 +5495,6 @@ function loadDialog(dialogDiv, callBack, autoShow, params, clearOnHide, closable
     load : function(data) {
       var contentWidget=dijit.byId(dialogDiv);
       contentWidget.set('content', data);
-      if(closable){
-        contentWidget.set('content', data);
-      }
       if (autoShow) {
         setTimeout("dijit.byId('" + dialogDiv + "').show();", 100);
       }
