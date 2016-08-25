9c54c7f012153f383eec84a1bffb70fc82da618d
babynus <babynus@f9421885-1815-481d-89b5-6d9585689fe2>
2016-08-25 21:07:38 +0000

    V5.5.0
    PBE

--- branches/V5.5/projeqtor/db/projeqtor_V5.5.0.sql
+++ branches/V5.5/projeqtor/db/projeqtor_V5.5.0.sql
@@ -239,7 +239,7 @@ INSERT INTO `${prefix}indicator` (`id`, `code`, `type`, `name`, `sortOrder`, `id
 (23, 'DELAY', 'delay', 'realEndDate', 370, 0, 'idBill'),
 (24, 'DELAY', 'delay', 'paymentDueDate', 380, 0, 'paymentDone'),
 (25, 'DELAY', 'delay', 'meetingDate', 390, 0, 'done'),
-(26, 'DELAY', 'delay', 'date', 400, 0, 'sendDate'),;
+(26, 'DELAY', 'delay', 'date', 400, 0, 'sendDate');
 
 INSERT INTO `${prefix}indicatorableindicator` (`idIndicatorable`, `nameIndicatorable`, `idIndicator`, `idle`) VALUES 
 ('14', 'Tender', '17', '0'),
@@ -255,6 +255,6 @@ INSERT INTO `${prefix}indicatorableindicator` (`idIndicatorable`, `nameIndicator
 ('19', 'Term', '23', '0'),
 ('20', 'Bill', '24', '0'),
 ('21', 'Meeting', '25', '0'),
-('20', 'Bill', '26', '0'),;
+('20', 'Bill', '26', '0');
 
 CREATE UNIQUE INDEX workReference ON `${prefix}work` (idAssignment, workDate);
