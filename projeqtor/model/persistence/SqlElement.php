a47442a95f850127b28298f7b80154e6b07201af
babynus <pascal.bernard@projeqtor.net>
2016-10-09 17:45:55 +0000

    V5.5.3
    PBE

--- projeqtor/model/persistence/SqlElement.php
+++ projeqtor/model/persistence/SqlElement.php
@@ -3046,11 +3046,22 @@ abstract class SqlElement {
 					$right='YES';
 				}
 			}
+		} else if (get_class($this)=='Affectation' and property_exists($this, '_automaticCreation') and $this->_automaticCreation) {
+		  $right='YES';
 		}
-		if ($right!='YES' and get_class($this)=='Project' and $this->idProject) {
-		  $proj=new Project($this->idProject,true);
-		  $right=securityGetAccessRightYesNo('menuProject', 'update', $proj);
-		} else if ($right!='YES' and get_class($this)=='Affectation' ) {
+		if ($right!='YES' and get_class($this)=='Project') {
+		  if ($this->idProject) {
+		    $proj=new Project($this->idProject,true);
+		    $right=securityGetAccessRightYesNo('menuProject', 'update', $proj);
+		  } else {
+		    //  $right=securityGetAccessRightYesNo('menu' . get_class($this), (($this->id)?'update':'create'), $this); // This shoulod be applied to avoid creation at root
+		    if ($this->id) {
+		      $right=securityGetAccessRightYesNo('menu' . get_class($this), 'update', $this);
+		    } else {
+          $right=securityGetAccessRightYesNo('menu' . get_class($this), 'create');
+		    }
+		  }
+		} else if ($right!='YES' and get_class($this)=='Affectation') {
 		  $prj=new Project($this->idProject,true);
 		  $right=securityGetAccessRightYesNo('menuProject', 'update', $prj);
 		} else if ($right!='YES') {
