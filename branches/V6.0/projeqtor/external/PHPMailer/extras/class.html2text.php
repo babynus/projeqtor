307959536019de0a2a1850717bc85230aabfb6ea
babynus <babynus@f9421885-1815-481d-89b5-6d9585689fe2>
2016-11-20 17:02:55 +0000

    V6.0.0 licence PBE

--- branches/V6.0/projeqtor/external/PHPMailer/extras/class.html2text.php
+++ branches/V6.0/projeqtor/external/PHPMailer/extras/class.html2text.php
@@ -7,17 +7,17 @@
  * This version from https://github.com/mtibben/html2text                *
  *                                                                       *
  * This script is free software; you can redistribute it and/or modify   *
- * it under the terms of the GNU General Public License as published by  *
+ * it under the terms of the GNU Affero General Public License as published by  *
  * the Free Software Foundation; either version 2 of the License, or     *
  * (at your option) any later version.                                   *
  *                                                                       *
- * The GNU General Public License can be found at                        *
+ * The GNU Affero General Public License can be found at                        *
  * http://www.gnu.org/copyleft/gpl.html.                                 *
  *                                                                       *
  * This script is distributed in the hope that it will be useful,        *
  * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
- * GNU General Public License for more details.                          *
+ * GNU Affero General Public License for more details.                          *
  *                                                                       *
  *************************************************************************/
 
