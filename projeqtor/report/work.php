b81c376b384c505921fd43785b93d78badbfb212
kevin <kevin.anoll@projeqtor.net>
2016-12-05 15:33:34 +0000

    V6.0 #2414 KAN

--- projeqtor/report/work.php
+++ projeqtor/report/work.php
@@ -104,13 +104,13 @@ if ( $paramResource=='') {
 }
 include "header.php";
 
-$where=getAccesRestrictionClause('Activity',false,false,true,true);
+$where=(getAccesRestrictionClause('Activity',false,false,true,true) ." or idResource=". getSessionUser()->id . ""); 
 //$where="1=1 ";
 $where.=($periodType=='week')?" and week='" . $periodValue . "'":'';
 $where.=($periodType=='month')?" and month='" . $periodValue . "'":'';
 $where.=($periodType=='year')?" and year='" . $periodValue . "'":'';
 if ($paramProject!='') {
-  $where.=  "and idProject in " . getVisibleProjectsList(true, $paramProject) ;
+  $where.=  "and (idProject in " . getVisibleProjectsList(true, $paramProject)." or idResource=". getSessionUser()->id .")"; 
 }
 $where.=($paramResource!='')?" and idResource='" . $paramResource . "'":'';
 $order="";
