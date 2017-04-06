e86ee3a696001817a3414531fd9477d5832b1d17
mehdi <mehdi.zenbil@projeqtor.net>
2017-04-06 11:35:38 +0000

    test

--- branches/V6.2/projeqtor/model/Action.php
+++ branches/V6.2/projeqtor/model/Action.php
@@ -23,7 +23,7 @@
  * about contributors at http://www.projeqtor.org 
  *     
  *** DO NOT REMOVE THIS NOTICE ************************************************/
-
+//test
 /** ============================================================================
  * Action is establised during meeting, to define an action to be followed.
  */ 
