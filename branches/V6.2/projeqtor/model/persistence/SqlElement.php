2c7ef50cacbdb5947e0086a92256853b247e1803
kevin <kevin@f9421885-1815-481d-89b5-6d9585689fe2>
2017-04-12 14:18:28 +0000

    6.2.0 #1789 kan

--- branches/V6.2/projeqtor/model/persistence/SqlElement.php
+++ branches/V6.2/projeqtor/model/persistence/SqlElement.php
@@ -3111,13 +3111,14 @@ abstract class SqlElement {
 		  	if (self::is_subclass_of($this,'PlanningElement')) {
 		  		$end=get_class($this).'_'.$end;
 		  		$start=get_class($this).'_'.$start;
+		  		debugLog($start);
 		  		$duration=get_class($this).'_'.str_replace('StartDate', 'Duration', $colName);
 		  	}
 		    $colScript .= '<script type="dojo/connect" event="onChange" args="evt">';
 		    $colScript .= "if(this.value){";
 		    $colScript .= "  var end = dijit.byId('$end');"; // $end will be replaced by value as enclosed by "
 		    $colScript .= "  if(end){";
-		    $colScript .= "    var dtStart = dijit.byId('$start').get('value'); "; // => rÃ©cupÃ©rer la date de this, qui est en string, au format Date javascrpit
+		    $colScript .= "    var dtStart = dijit.byId('$start').get('value'); "; // => rÃƒÂ©cupÃƒÂ©rer la date de this, qui est en string, au format Date javascrpit
 		    $colScript .= "    end.constraints.min=dtStart;";
 		    $colScript .= "    if (! end.get('value') ) {";
 		    if (!substr($colName, -7)=="EisDate") $colScript .= "      end.set('value',dtStart);";
