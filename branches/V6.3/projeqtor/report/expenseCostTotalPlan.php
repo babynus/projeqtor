2f33feb4d7d0fbd4b26885d0018565cb40c26e21
kevin <kevin.anoll@projeqtor.net>
2017-05-04 07:16:02 +0000

    V6.3 #2697,#1817 KAN

--- branches/V6.3/projeqtor/report/expenseCostTotalPlan.php
+++ branches/V6.3/projeqtor/report/expenseCostTotalPlan.php
@@ -310,11 +310,22 @@ if (! testGraphEnabled()) { return;}
   include("../external/pChart/pData.class");  
   include("../external/pChart/pChart.class");  
 $dataSet=new pData;
+$width=700;
+$graph = new pChart($width,260);
 $nbItem=0;
 foreach($sumProj as $id=>$vals) {
   $dataSet->AddPoint($vals,$id);
   $dataSet->SetSerieName($tab[$id]['name'],$id);
   $dataSet->AddSerie($id);
+  $proje=new Project($id);
+  $projCol = $proje->color;
+  $projectColor=$proje->getColor();
+  $colorProj=hex2rgb($projectColor);
+  if($projCol){
+    $graph->setColorPalette($nbItem,$colorProj['R'],$colorProj['G'],$colorProj['B']);
+  } else {
+    $graph->setColorPalette($nbItem,$rgbPalette[($nbItem % 12)]['R'],$rgbPalette[($nbItem % 12)]['G'],$rgbPalette[($nbItem % 12)]['B']);
+  }
   $nbItem++;
 }
 $arrLabel=array();
@@ -323,11 +334,6 @@ foreach($arrDates as $date){
 }
 $dataSet->AddPoint($arrLabel,"dates");  
 $dataSet->SetAbsciseLabelSerie("dates");   
-$width=700;
-$graph = new pChart($width,260);  
-for ($i=0;$i<=$nbItem;$i++) {
-  $graph->setColorPalette($i,$rgbPalette[($i % 12)]['R'],$rgbPalette[($i % 12)]['G'],$rgbPalette[($i % 12)]['B']);
-}
 $graph->setFontProperties("../external/pChart/Fonts/tahoma.ttf",10);
 $graph->drawRoundedRectangle(5,5,$width-5,258,5,230,230,230);  
 $graph->setGraphArea(40,30,$width-200,200);  
