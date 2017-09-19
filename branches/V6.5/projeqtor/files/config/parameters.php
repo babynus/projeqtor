9906d6a6be1337aa9653ba4799a355cb55292ed2
kevin <kevin@f9421885-1815-481d-89b5-6d9585689fe2>
2017-09-19 07:18:53 +0000

    KAN 6.5

--- branches/V6.5/projeqtor/files/config/parameters.php
+++ branches/V6.5/projeqtor/files/config/parameters.php
@@ -5,7 +5,7 @@
 // ========== Database configuration =====================================================
 // --- MySql Degfault
 $paramDbType='mysql'; $paramDbPort='3306'; $paramDbUser='root'; $paramDbPassword='mysql';
-$paramDbName='projeqtor_v64';$paramDbPrefix='';
+$paramDbName='projeqtor_v65';$paramDbPrefix='';
 // --- PostgreSql Default
 //$paramDbType='pgsql'; $paramDbPort='5432'; $paramDbUser='projeqtor_maaf'; $paramDbPassword='projeqtor';
 //$paramDbName='projeqtor';$paramDbPrefix=''; 
