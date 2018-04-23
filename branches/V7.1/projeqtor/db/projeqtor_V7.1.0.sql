a92efc020e728d0b4194c79b6747ab35b480c1b8
gautier <gautier@f9421885-1815-481d-89b5-6d9585689fe2>
2018-04-23 15:54:16 +0000

    GPA V7.1 #ResourceTeam

--- branches/V7.1/projeqtor/db/projeqtor_V7.1.0.sql
+++ branches/V7.1/projeqtor/db/projeqtor_V7.1.0.sql
@@ -16,3 +16,13 @@ INSERT INTO `${prefix}reportparameter` (`idReport`, `name`, `paramType`, `sortOr
 
 INSERT INTO `${prefix}habilitationreport` (`idProfile`, `idReport`, `allowAccess`) VALUES 
 (1, 83, 1);
+
+-- gautier resourceTeam
+INSERT INTO `${prefix}menu` (`id`,`name`, `idMenu`, `type`, `sortOrder`, `level`, `idle`, `menuClass`) VALUES
+(188,'menuResourceTeam', 14, 'object', 505, 'ReadWriteEnvironment', 0, 'Work EnvironmentalParameter');
+INSERT INTO `${prefix}habilitation` (`idProfile`, `idMenu`, `allowAccess`) VALUES 
+(1,188,1);
+INSERT INTO `${prefix}accessright` (`idProfile`, `idMenu`, `idAccessProfile`) VALUES 
+(1,188, 1000001);
+
+ALTER TABLE `${prefix}resource` ADD `isResourceTeam` int(1) UNSIGNED DEFAULT 0;
