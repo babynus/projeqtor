4ed127b621fc4cc93bf2511bd7a5b64b729b98a9
gautier <gautier.pradines@projeqtor.net>
2018-10-08 12:15:44 +0000

    GPA #3604

--- projeqtor/model/RequirementMain.php
+++ projeqtor/model/RequirementMain.php
@@ -100,7 +100,7 @@ class RequirementMain extends SqlElement {
   public $_Dependency_Predecessor=array();
   public $_sec_successor;
   public $_Dependency_Successor=array();
-  public $_sec_TestCaseList;
+  public $_sec_Link_TestCase;
   public $_Link_TestCase=array();
   public $_sec_Link;
   public $_Link=array();
