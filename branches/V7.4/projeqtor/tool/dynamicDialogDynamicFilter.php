d9ff91972e9e1082a385c45a38d8cb24f1a19803
damian <damian.sanchez@projeqtor.net>
2019-04-12 15:12:06 +0000

    gautier

--- branches/V7.4/projeqtor/tool/dynamicDialogDynamicFilter.php
+++ branches/V7.4/projeqtor/tool/dynamicDialogDynamicFilter.php
@@ -39,7 +39,8 @@ Security::checkValidClass($objectClass);
            foreach ($filter->_FilterCriteriaArray as $filterCriteria) {
            	if ($filterCriteria->isDynamic!="1") continue;
            	//gautier
-           	if(trim($filterCriteria->dispOperator) == "<= today" or trim($filterCriteria->dispOperator)== ">= today"){
+           	$today = i18n('today');
+           	if(trim($filterCriteria->dispOperator) == "<= ".$today or trim($filterCriteria->dispOperator)== ">= ".$today){
            	  $filterCriteria->dispOperator .= ' + ';
            	}
            ?>
