2a73ac52bab09dcd768f50ac4ed93ccb4405d002
gautier <gautier.pradines@projeqtor.net>
2019-08-01 16:10:14 +0000

    GPA

--- branches/V8.2/projeqtor/model/DataCloning.php
+++ branches/V8.2/projeqtor/model/DataCloning.php
@@ -406,7 +406,7 @@ class DataCloning extends SqlElement{
 	  	//end exception
 	  	if($element->isDir()){
 	  		mkdir($dir_dest . DIRECTORY_SEPARATOR . $iterator->getSubPathName());
-	  	} else{
+	  	}else{
 	  		if(($element->getBasename()=="parametersLocation.php")){
 	  			$paramLocation =  $dir_dest . DIRECTORY_SEPARATOR . $iterator->getSubPathName();
 	  		}
@@ -505,8 +505,47 @@ class DataCloning extends SqlElement{
 	  $dataCloning = new DataCloning($id);
 	  $dataCloning->idle = 1;
 	  $dataCloning->deletedDate = date('Y-m-d H:i');
+	  
+	  $dir= dirname(__DIR__).'/simulation/'.$dataCloning->nameDir;
+	  $dataCloning->remove_dir($dir,$dataCloning);
 	  $dataCloning->save();
 	}
+	
+public static	function remove_dir($directory,$dataCloning,$empty = false) {
+  if(substr($directory,-1) == "/") {
+    $directory = substr($directory,0,-1);
+  }
+
+  if(!file_exists($directory) || !is_dir($directory)) {
+    return false;
+  } elseif(!is_readable($directory)) {
+    return false;
+  } else {
+    $directoryHandle = opendir($directory);
+
+    while ($contents = readdir($directoryHandle)) {
+      if($contents != '.' && $contents != '..') {
+        $path = $directory . "/" . $contents;
+
+        if(is_dir($path)) {
+          $dataCloning->remove_dir($path,$dataCloning);
+        } else {
+          unlink($path);
+        }
+      }
+    }
+
+    closedir($directoryHandle);
+
+    if($empty == false) {
+      if(!rmdir($directory)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
 	
 	public static function connectTestSimu($dbName){
 		$dbType=Parameter::getGlobalParameter('paramDbType');
