6415f0ec60ec58c1902c35c82f739636a84e1295
florent <florent.arnaud@projeqtor.net>
2019-09-03 10:49:01 +0000

    FAR ticket 4102

--- branches/V8.2/projeqtor/view/portfolioPlanningMain.php
+++ branches/V8.2/projeqtor/view/portfolioPlanningMain.php
@@ -53,7 +53,9 @@
   $currentScreen='PortfolioPlanning';
   $positionListDiv=changeLayoutObjectDetail($paramScreen,$paramLayoutObjectDetail);
   $positonRightDiv=changeLayoutActivityStream($paramRightDiv);
-  $rightHeightPortfolioPlanning=heightLaoutActivityStream($currentScreen);
+  if($positonRightDiv=="bottom"){
+    $rightHeightPortfolioPlanning=heightLaoutActivityStream($currentScreen);
+  }
   if($positionListDiv=='left'){
     $widthListDiv='65%';
     $widthDetailDiv='25%';
@@ -66,32 +68,44 @@
 <input type="hidden" name="objectClassManual" id="objectClassManual" value="PortfolioPlanning" />
 <input type="hidden" name="portfolioPlanning" id="portfolioPlanning" value="true" />
 <div id="mainDivContainer" class="container" dojoType="dijit.layout.BorderContainer">
-  <div id="listDiv" dojoType="dijit.layout.ContentPane" region="<?php echo $positionListDiv?>" splitter="true" style="width:<?php echo $widthListDiv?>;height:<?php echo $listHeight;?>;">
+  <div id="listDiv" dojoType="dijit.layout.ContentPane" region="<?php echo $positionListDiv?>" splitter="true" 
+   style="<?php if($positionListDiv=='top'){echo "height:".$listHeight;}else{ echo "width:".$widthListDiv;}?>">
     <script type="dojo/connect" event="resize" args="evt">
          if (switchedMode) return;
+          var paramDiv=<?php echo json_encode($positionListDiv); ?>;
+           if(paramDiv=="top"){
              saveDataToSession("contentPaneTopPortfolioPlanningDivHeight", dojo.byId("listDiv").offsetHeight, true);
+           }
     </script>
    <?php include 'portfolioPlanningList.php'?>
   </div>
   <div id="contentDetailDiv" dojoType="dijit.layout.ContentPane" region="center"   style="width:<?php echo $widthDetailDiv; ?>;">
+      <script type="dojo/connect" event="resize" args="evt">
+           var paramDiv=<?php echo json_encode($positionListDiv); ?>;
+           if(paramDiv=="top"){
+              saveDataToSession("contentPaneDetailDivHeight<?php echo $currentScreen;?>", dojo.byId("detailDiv").offsetHeight, true); 
+            }
+      </script>
   <div class="container" dojoType="dijit.layout.BorderContainer"  liveSplitters="false">
     <div id="detailDiv" dojoType="dijit.layout.ContentPane" region="center" style="width:<?php echo $widthDetailDiv?>">
       <div id="detailBarShow" class="dijitAccordionTitle" onMouseover="hideList('mouse');" onClick="hideList('click');">
         <div id="detailBarIcon" align="center"></div>
       </div>
         <?php $noselect=true; //include 'objectDetail.php'; ?>
-      <script type="dojo/connect" event="resize" args="evt">
-          saveDataToSession("contentPaneDetailDivHeight<?php echo $currentScreen;?>", dojo.byId("detailDiv").offsetHeight, true);
-      </script>
      </div>
-     <div id="detailRightDiv" dojoType="dijit.layout.ContentPane" region="<?php echo $positonRightDiv; ?>" splitter="true" style="width:<?php echo $rightWidthPortfolioPlanning;?>;height:<?php echo $rightHeightPortfolioPlanning;?>">
+     <div id="detailRightDiv" dojoType="dijit.layout.ContentPane" region="<?php echo $positonRightDiv; ?>" splitter="true" 
+     style="<?php if($positonRightDiv=="bottom"){echo "height:".$rightHeightPortfolioPlanning;}else{ echo "width:".$rightWidthPortfolioPlanning;}?>">
               <script type="dojo/connect" event="resize" args="evt">
-                  saveDataToSession("contentPaneRightDetailDivWidthPortfolioPlanning", dojo.byId("detailRightDiv").offsetWidth, true);
-                  saveDataToSession("contentPaneRightDetailDivHeightPortfolioPlanning", dojo.byId("detailRightDiv").offsetHeight, true);
-                  var newWidth=dojo.byId("detailRightDiv").offsetWidth;
-                  dojo.query(".activityStreamNoteContainer").forEach(function(node, index, nodelist) {
-                  node.style.maxWidth=(newWidth-30)+"px";
-                  });
+                  var paramDiv=<?php echo json_encode($positonRightDiv); ?>;
+                  if(paramDiv=='trailing'){
+                    saveDataToSession("contentPaneRightDetailDivWidthPortfolioPlanning", dojo.byId("detailRightDiv").offsetWidth, true);
+                    var newWidth=dojo.byId("detailRightDiv").offsetWidth;
+                    dojo.query(".activityStreamNoteContainer").forEach(function(node, index, nodelist) {
+                    node.style.maxWidth=(newWidth-30)+"px";
+                    });
+                  }else{
+                    saveDataToSession("contentPaneRightDetailDivHeightPortfolioPlanning", dojo.byId("detailRightDiv").offsetHeight, true);
+                  }
               </script>
               <script type="dojo/connect" event="onLoad" args="evt">
                 scrollInto();
--- branches/V8.2/projeqtor/view/resourcePlanningMain.php
+++ branches/V8.2/projeqtor/view/resourcePlanningMain.php
@@ -52,7 +52,9 @@
   $currentScreen='ResourcePlanning';
   $positionListDiv=changeLayoutObjectDetail($paramScreen,$paramLayoutObjectDetail);
   $positonRightDiv=changeLayoutActivityStream($paramRightDiv);
-  $rightHeightResourcePlanning=heightLaoutActivityStream($currentScreen);
+  if($positonRightDiv=="bottom"){
+    $rightHeightResourcePlanning=heightLaoutActivityStream($currentScreen);
+  }
   if($positionListDiv=='left'){
     $widthListDiv='65%';
     $widthDetailDiv='25%';
@@ -65,32 +67,46 @@
 <input type="hidden" name="objectClassManual" id="objectClassManual" value="ResourcePlanning" />
 <input type="hidden" name="resourcePlanning" id="resourcePlanning" value="true" />
 <div id="mainDivContainer" class="container" dojoType="dijit.layout.BorderContainer">
-  <div id="listDiv" dojoType="dijit.layout.ContentPane" region="<?php echo $positionListDiv?>" splitter="true" style="width:<?php echo $widthListDiv?>;height:<?php echo $listHeight;?>;">
+  <div id="listDiv" dojoType="dijit.layout.ContentPane" region="<?php echo $positionListDiv?>" splitter="true" 
+   style="<?php if($positionListDiv=='top'){echo "height:".$listHeight;}else{ echo "width:".$widthListDiv;}?>">
     <script type="dojo/connect" event="resize" args="evt">
          if (switchedMode) return;
-              saveDataToSession("contentPaneTopResourcePlanningDivHeight", dojo.byId("listDiv").offsetHeight, true);
+         var paramDiv=<?php echo json_encode($positionListDiv); ?>;
+           if(paramDiv=="top"){
+             saveDataToSession("contentPaneTopResourcePlanningDivHeight", dojo.byId("listDiv").offsetHeight, true);
+          }
     </script>
    <?php include 'resourcePlanningList.php'?>
   </div>
   <div id="contentDetailDiv" dojoType="dijit.layout.ContentPane" region="center"   style="width:<?php echo $widthDetailDiv; ?>;">
+      <script type="dojo/connect" event="resize" args="evt">
+           var paramDiv=<?php echo json_encode($positionListDiv); ?>;
+           if(paramDiv=="top"){
+            saveDataToSession("contentPaneDetailDivHeight<?php echo $currentScreen;?>", dojo.byId("detailDiv").offsetHeight, true);
+           }
+      </script>
   <div class="container" dojoType="dijit.layout.BorderContainer"  liveSplitters="false">
       <div id="detailDiv" dojoType="dijit.layout.ContentPane" region="center"  style="width:<?php echo $widthDetailDiv?>">
         <div id="detailBarShow" class="dijitAccordionTitle" onMouseover="hideList('mouse');" onClick="hideList('click');">
           <div id="detailBarIcon" align="center"></div>
         </div>
         <?php $noselect=true; //include 'objectDetail.php'; ?>
-        <script type="dojo/connect" event="resize" args="evt">
-          saveDataToSession("contentPaneDetailDivHeight<?php echo $currentScreen;?>", dojo.byId("detailDiv").offsetHeight, true);
-        </script>
+
       </div>
-      <div id="detailRightDiv" dojoType="dijit.layout.ContentPane" region="<?php echo $positonRightDiv; ?>" splitter="true" style="width:<?php echo $rightWidthResourcePlanning;?>;height:<?php echo $rightHeightResourcePlanning;?>">
+      <div id="detailRightDiv" dojoType="dijit.layout.ContentPane" region="<?php echo $positonRightDiv; ?>" splitter="true" 
+      style="<?php if($positonRightDiv=="bottom"){echo "height:".$rightHeightResourcePlanning;}else{ echo "width:".$rightWidthResourcePlanning;}?>">
               <script type="dojo/connect" event="resize" args="evt">
-                  saveDataToSession("contentPaneRightDetailDivWidthResourcePlanning", dojo.byId("detailRightDiv").offsetWidth, true);
-                  saveDataToSession("contentPaneRightDetailDivHeightResourcePlanning", dojo.byId("detailRightDiv").offsetHeight, true);
-                  var newWidth=dojo.byId("detailRightDiv").offsetWidth;
-                  dojo.query(".activityStreamNoteContainer").forEach(function(node, index, nodelist) {
-                  node.style.maxWidth=(newWidth-30)+"px";
-                  });
+               var paramDiv=<?php echo json_encode($positonRightDiv); ?>;
+                  if(paramDiv=='trailing'){
+                    saveDataToSession("contentPaneRightDetailDivWidthResourcePlanning", dojo.byId("detailRightDiv").offsetWidth, true);
+                    var newWidth=dojo.byId("detailRightDiv").offsetWidth;
+                    dojo.query(".activityStreamNoteContainer").forEach(function(node, index, nodelist) {
+                    node.style.maxWidth=(newWidth-30)+"px";
+                    });
+                  }else{
+                    saveDataToSession("contentPaneRightDetailDivHeightResourcePlanning", dojo.byId("detailRightDiv").offsetHeight, true);
+                  }
+                 
               </script>
               <script type="dojo/connect" event="onLoad" args="evt">
                 scrollInto();
