c28cdd19ad6356ae20d53d3ad82e849832f77eb5
florent <florent.arnaud@projeqtor.net>
2019-09-06 10:50:31 +0000

    FAR ticket 4102

--- branches/V8.2/projeqtor/view/versionsPlanningMain.php
+++ branches/V8.2/projeqtor/view/versionsPlanningMain.php
@@ -12,7 +12,6 @@ $screenHeight=getSessionValue('screenHeight');
 if ($screenHeight and $topDetailDivHeight>$screenHeight-300) {
 	$topDetailDivHeight=$screenHeight-300;
 }
-
 //florent
 $paramScreen=RequestHandler::getValue('paramScreen');
 $paramLayoutObjectDetail=RequestHandler::getValue('paramLayoutObjectDetail');
@@ -58,41 +57,14 @@ $tableWidth=WidthDivContentDetail($positionListDiv,$currentScreen);
               saveDataToSession("contentPaneDetailDivWidth<?php echo $currentScreen;?>", dojo.byId("contentDetailDiv").offsetWidth, true);
             }
       </script>
-  <div class="container" dojoType="dijit.layout.BorderContainer"  liveSplitters="false">
+      <div class="container" dojoType="dijit.layout.BorderContainer"  liveSplitters="false">
         <div id="detailBarShow" class="dijitAccordionTitle" onMouseover="hideList('mouse');" onClick="hideList('click');"
           <?php if (RequestHandler::isCodeSet('switchedMode') and RequestHandler::getValue('switchedMode')=='on') echo ' style="display:block;"'?>>
           <div id="detailBarIcon" align="center"></div>
         </div>
-      <div id="detailDiv" dojoType="dijit.layout.ContentPane" region="center" >
-        <?php $noselect=true; //include 'objectDetail.php'; ?>
-
-      </div>
-      <div id="detailRightDiv" dojoType="dijit.layout.ContentPane" region="<?php echo $positonRightDiv; ?>" splitter="true" 
-      style="<?php if($positonRightDiv=="bottom"){echo "height:".$rightHeightVersionsPlannig;}else{ echo "width:".$rightWidthVersionsPlannig;}?>">
-              <script type="dojo/connect" event="resize" args="evt">
-               var paramDiv=<?php echo json_encode($positonRightDiv); ?>;
-               var paramMode=<?php echo json_encode(Parameter::getUserParameter('paramScreen')); ?>;
-                  if(paramDiv=='trailing' && paramMode!='5'){
-                    saveDataToSession("contentPaneRightDetailDivWidth<?php echo $currentScreen;?>", dojo.byId("detailRightDiv").offsetWidth, true);
-                    var newWidth=dojo.byId("detailRightDiv").offsetWidth;
-                    dojo.query(".activityStreamNoteContainer").forEach(function(node, index, nodelist) {
-                    node.style.maxWidth=(newWidth-30)+"px";
-                    });
-                  }else if(paramMode!='5'){
-                    saveDataToSession("contentPaneRightDetailDivHeight<?php echo $currentScreen;?>", dojo.byId("detailRightDiv").offsetHeight, true);
-                  }
-                 
-              </script>
-              <script type="dojo/connect" event="onLoad" args="evt">
-                scrollInto();
-	         </script>
-              <?php include 'objectStream.php'?>
-          </div>
+        <div id="detailDiv" dojoType="dijit.layout.ContentPane" region="center" >
+          <?php $noselect=true; //include 'objectDetail.php'; ?>
+        </div>
+    </div>
   </div>
-  </div>
-  
-  
-  
-  
-  
 </div>
