6bb5446f56df63e8535ff757659af764412fc07a
damian <damian.sanchez@projeqtor.net>
2019-09-13 14:42:34 +0000

    pour florent

--- branches/V8.2/projeqtor/view/objectMain.php
+++ branches/V8.2/projeqtor/view/objectMain.php
@@ -97,7 +97,14 @@ use PhpOffice\PhpPresentation\Shape\RichText\Paragraph;
           console.log(width);
           dojo.query(".allSearchTD").forEach(function(node, index, nodelist) { node.style.display="table-cell";});
           var refWidth=50;
-          if (width<1200) {
+          if (width<1650) {
+            dojo.query(".clientSearchTD").forEach(function(node, index, nodelist) { node.style.display="none";});
+          }
+          if (width<1300) {
+            dojo.query(".parentBudgetSearchTD").forEach(function(node, index, nodelist) { node.style.display="none";});
+            dojo.query(".clientSearchTD").forEach(function(node, index, nodelist) { node.style.display="none";});
+          }
+          if (width<1100) {
             refWidth=25;
           }
           dojo.query("#widget_listNameFilter").forEach(function(node, index, nodelist) { node.style.width=(refWidth*2)+"px";});
@@ -108,6 +115,10 @@ use PhpOffice\PhpPresentation\Shape\RichText\Paragraph;
             dojo.query(".typeSearchTD").forEach(function(node, index, nodelist) { node.style.display="none";});
           }
           if (width<800) {
+            dojo.query(".idSearchTD").forEach(function(node, index, nodelist) { node.style.display="none";});
+            dojo.query(".resetSearchTD").forEach(function(node, index, nodelist) { node.style.display="none";});
+          }
+          if (width<700) {
             dojo.query(".idSearchTD").forEach(function(node, index, nodelist) { node.style.display="none";});
           }
           if (width<600) {
