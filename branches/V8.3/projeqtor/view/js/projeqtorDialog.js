b3a4b85653e5b672b3ed148522cc46abd438ee66
damian <damian@f9421885-1815-481d-89b5-6d9585689fe2>
2019-11-05 13:37:50 +0000

    DSAN
    #4294

--- branches/V8.3/projeqtor/view/js/projeqtorDialog.js
+++ branches/V8.3/projeqtor/view/js/projeqtorDialog.js
@@ -750,9 +750,7 @@ function selectDetailItem(selectedValue, lastSavedName) {
   		combo.set("value", idFldVal);
   		//}
     }else{
-      if(idFldVal.indexOf('_')>=0) 
-        idFldVal=idFldVal.split('_');
-  		combo.set("value", idFldVal);
+  	  combo.set("value", idFldVal);
   	}
   }
   hideDetail();
