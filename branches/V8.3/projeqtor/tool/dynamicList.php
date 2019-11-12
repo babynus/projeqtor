0fc165129b972de98b1fd2aeb26810e69cee0636
gautier <gautier@f9421885-1815-481d-89b5-6d9585689fe2>
2019-11-12 14:39:44 +0000

    GPA #4290

--- branches/V8.3/projeqtor/tool/dynamicList.php
+++ branches/V8.3/projeqtor/tool/dynamicList.php
@@ -62,7 +62,7 @@ class="selectList" >
      $wbsTest=$wbs;
      $level=1;
      while (strlen($wbsTest)>3) {
-       $wbsTest=substr($wbsTest,0,strlen($wbsTest)-4);
+       $wbsTest=substr($wbsTest,0,strlen($wbsTest)-6);
        if (array_key_exists($wbsTest, $wbsLevelArray)) {
          $level=$wbsLevelArray[$wbsTest]+1;
          $wbsTest="";
