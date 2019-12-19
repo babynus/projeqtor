b79f1e2324279649633562a6f90047049110fee3
gautier <gautier.pradines@projeqtor.net>
2019-12-19 14:02:06 +0000

    GPA

--- projeqtor/tool/formatter.php
+++ projeqtor/tool/formatter.php
@@ -442,8 +442,8 @@ function formatCommentThumb($comment,$img=null) {
 
 function getMonthName($month,$maxLength=0) {
   global $monthArray;
-  if (! $month or $month==0 or !isset($monthArray)) return '';
-  if (count($monthArray)==0) {
+  if (! $month or $month==0) return '';
+  if (!isset($monthArray) or count($monthArray)==0) {
     $monthArray=array(i18n("January"),i18n("February"),i18n("March"),
       i18n("April"), i18n("May"),i18n("June"),
       i18n("July"), i18n("August"), i18n("September"),
