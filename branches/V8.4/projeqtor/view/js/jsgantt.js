1f69d101ff831b524e167281b7f21c9e6d17ec87
florent <florent@f9421885-1815-481d-89b5-6d9585689fe2>
2020-01-13 13:51:01 +0000

    FAR ticket 44333

--- branches/V8.4/projeqtor/view/js/jsgantt.js
+++ branches/V8.4/projeqtor/view/js/jsgantt.js
@@ -2347,7 +2347,8 @@ function dependencyRightClick(evt){
   divNode.style.left=((evt.pageX)+7)+"px";
   divNode.style.top=evt.pageY+"px";
   var streamWitdh=0;
-  if (dojo.byId("detailRightDiv")) {
+  //florent 4433
+  if (dojo.byId("detailRightDiv") && (dojo.byId("mainDivContainer").style.width !=  dojo.byId("detailRightDiv").style.width )) {
     streamWitdh=parseInt(dojo.byId("detailRightDiv").style.width)+5;
   }
   if(evt.pageY>divRightGanttChartHeight+divRightGanttChartTop-editDependencyDivHeight){
