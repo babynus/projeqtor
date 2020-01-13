a6aef52045ccb0c2d85ea56d200023f8f5ea7d1b
florent <florent.arnaud@projeqtor.net>
2020-01-13 13:31:08 +0000

    FAR ticket 4433

--- branches/V8.3/projeqtor/view/js/jsgantt.js
+++ branches/V8.3/projeqtor/view/js/jsgantt.js
@@ -2347,6 +2347,7 @@ function dependencyRightClick(evt){
   divNode.style.left=((evt.pageX)+7)+"px";
   divNode.style.top=evt.pageY+"px";
   var streamWitdh=0;
+  //florent
   if (dojo.byId("detailRightDiv") && (dojo.byId("mainDivContainer").style.width !=  dojo.byId("detailRightDiv").style.width )) {
     streamWitdh=parseInt(dojo.byId("detailRightDiv").style.width)+5;
   }
