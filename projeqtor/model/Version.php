144cd035ccd87df23632bc942b51e021a6d31ab1
florent <florent.arnaud@projeqtor.net>
2020-01-14 18:21:32 +0000

    FAR ticket 4435

--- projeqtor/model/Version.php
+++ projeqtor/model/Version.php
@@ -589,8 +589,9 @@ static protected function drawProductUsingComponentVersion($class, $id)
   protected static $existingIDs = array();
   protected static $tabDirectChild = array();
   
-  public function treatmentVersionPlanning ($parentVersion) {
-    
+  public function treatmentVersionPlanning ($parentVersion,$displayComponent,$compWithAct) {
+    $hideversionsWithoutActivity=Parameter::getUserParameter('versionsWithoutActivity');
+    $displayProductversionActivity = Parameter::getUserParameter('planningVersionDisplayProductVersionActivity');
     $planningVersionShowClosed = Parameter::getUserParameter('planningVersionShowClosed');
     if ($this->directChild($parentVersion)) {
       $this->displayVersion($parentVersion);
@@ -598,12 +599,21 @@ static protected function drawProductUsingComponentVersion($class, $id)
       if ($this->hasChild()) {
         foreach (ProductVersionStructure::getComposition($this->id) as $key => $idComponentVersion) {
           $componentVersion = new ComponentVersion($idComponentVersion);
-          
-
           $hide=SqlList::getFieldFromId('ComponentVersionType', $componentVersion->idComponentVersionType, 'lockUseOnlyForCC');
+          
+          if($displayProductversionActivity == 1  and $hideversionsWithoutActivity== 1){
+            foreach ($displayComponent as $id){
+              if($id==$idComponentVersion){
+                $hide=1;
+              }
+            }
+            if(in_array($idComponentVersion, $compWithAct)){
+              $hide=0;
+            }
+          }
           if ($hide!=1 and ( $componentVersion->idle == 0  or $planningVersionShowClosed == 1) ){
             if ($componentVersion->isDelivered == 0 or $planningVersionShowClosed == 1 )
-              $componentVersion->treatmentVersionPlanning($this);
+              $componentVersion->treatmentVersionPlanning($this,$displayComponent,$compWithAct);
           }
         }
       }
