4772ca414a88fcd9206446c90a406b49d5647fe4
florent <florent@f9421885-1815-481d-89b5-6d9585689fe2>
2020-01-14 10:26:42 +0000

    FAR

--- branches/V8.4/projeqtor/tool/displayFilterList.php
+++ branches/V8.4/projeqtor/tool/displayFilterList.php
@@ -50,7 +50,7 @@ if (! RequestHandler::isCodeSet('filterObjectClass')) {
   $filterObjectClass=RequestHandler::getValue('filterObjectClass');
 }
 if (!isset($objectClass) or !$objectClass) $objectClass=$filterObjectClass;
-if ($objectClass=='Planning' or $objectClass=='GlobalPlanning' or $objectClass=='VersionsPlanning' or $objectClass='ResourcePlanning') $objectClass='Activity';
+if ($objectClass=='Planning' or $objectClass=='GlobalPlanning' or $objectClass=='VersionsPlanning' or $objectClass=='ResourcePlanning') $objectClass='Activity';
 else if (substr($objectClass,0,7)=='Report_') $objectClass=substr($objectClass,7);
 Security::checkValidClass($objectClass);
 if (RequestHandler::isCodeSet('context')) $context=RequestHandler::getValue('context');
