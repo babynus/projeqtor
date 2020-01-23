0df1f2a464cd98efa993bdd094ea0c4db90f4a2a
gautier <gautier.pradines@projeqtor.net>
2020-01-23 15:46:28 +0000

    GPA #4402

--- branches/V8.4/projeqtor/view/objectList.php
+++ branches/V8.4/projeqtor/view/objectList.php
@@ -489,47 +489,9 @@ if (property_exists($objectClass,'idStatus')) {
                 <?php echo autoOpenFilteringSelect();?> 
                 data-dojo-props="queryExpr: '*${0}*',autoComplete:false"
                 id="listBudgetParentFilter" name="listBudgetParentFilter" style="width:<?php echo $referenceWidth*4;?>px" value="<?php if(!$comboDetail and sessionValueExists('listBudgetParentFilter')){ echo getSessionValue('listBudgetParentFilter'); }?>" >
-                  <option value=""></option>
                   <?php 
                    //gautier #indentBudget
-                   // htmlDrawOptionForReference('idBudgetItemParent',$budgetParent,$obj,false);
-                      $sepChar=Parameter::getUserParameter('projectIndentChar');
-                      if (!$sepChar) $sepChar='__';
-                      $wbsLevelArray=array();
-                      $wbsList=SqlList::getList('Budget','bbsSortable',null,true);
-                      $order="bbsSortable asc";
-                      $budget=new Budget();
-                      $where = null;
-                      if(sessionValueExists('listShowIdle'.$objectClass)){  
-                        if(getSessionValue('listShowIdle'.$objectClass)!= "on"){
-                          $where = ' idle=0 ';
-                        }
-                      }
-                      $list=$budget->getSqlElementsFromCriteria(null,false,$where,$order,null,true);
-                      foreach ($list as $projOb){
-                        if (isset($wbsList[$projOb->id])) {
-                          $wbs=$wbsList[$projOb->id];
-                        } else {
-                          $wbs='';
-                        }
-                        $wbsTest=$wbs;
-                        $level=1;
-                        while (strlen($wbsTest)>3) {
-                          $wbsTest=substr($wbsTest,0,strlen($wbsTest)-6);
-                          if (array_key_exists($wbsTest, $wbsLevelArray)) {
-                            $level=$wbsLevelArray[$wbsTest]+1;
-                            $wbsTest="";
-                          }
-                        }
-                        $wbsLevelArray[$wbs]=$level;
-                        $sep='';
-                        for ($i=1; $i<$level;$i++) {$sep.=$sepChar;}
-                        $val = $sep.$projOb->name;
-                        ?> 
-                        <option value="<?php echo $projOb->id; ?>"><?php echo $val; ?></option>      
-                       <?php
-                     }
-                    ?>
+                   htmlDrawOptionForReference('idBudgetItem',$budgetParent,$obj,false);?>
                   <script type="dojo/method" event="onChange" >
                     refreshJsonList('<?php echo $objectClass;?>');
                   </script>
@@ -950,6 +912,7 @@ if (property_exists($objectClass,'idStatus')) {
                 type="checkbox" id="listShowIdle" name="listShowIdle" <?php if(!$comboDetail and sessionValueExists('listShowIdle'.$objectClass)){   if(getSessionValue('listShowIdle'.$objectClass)== "on"){ ?>checked="checked" <?php }}?>>
                 <script type="dojo/method" event="onChange" >
                   refreshJsonList('<?php echo $objectClass;?>');
+                  <?php if($objectClass=='Budget'){?> refreshList('idBudgetParent','showIdle',dijit.byId('listShowIdle').get('value'),dijit.byId('listBudgetParentFilter').get('value'),'listBudgetParentFilter'); <?php } ?>
                 </script>
               </div>&nbsp;
             </td>
