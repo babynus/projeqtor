0da27f2a7f34836d4b2e9c4fd97f631396d3bc3d
florent <florent.arnaud@projeqtor.net>
2020-01-30 16:02:49 +0000

    FAR ticket 4397

--- branches/V8.4/projeqtor/tool/jsonQuery.php
+++ branches/V8.4/projeqtor/tool/jsonQuery.php
@@ -416,9 +416,6 @@
 	      //$sp=preg_split('field=', $val);
 	      //$sp=explode('field=', $val);
 	      $fld=htmlExtractArgument($val, 'field');
-// 	      if($fld="unitDurationContract" or $fld="unitDurationNotice"){
-// 	        $fld="id".$fld;
-// 	      }  
 	      if ($fld) {
 	        $numField+=1;    
 	        $formatter[$numField]=htmlExtractArgument($val, 'formatter');
