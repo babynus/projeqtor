e0504eaf09b6f3ecd8a9000e39fc62ff901b0b58
gautier <gautier@f9421885-1815-481d-89b5-6d9585689fe2>
2020-03-24 15:01:44 +0000

    GPA #3287

--- branches/V8.5/projeqtor/view/js/projeqtor.js
+++ branches/V8.5/projeqtor/view/js/projeqtor.js
@@ -649,11 +649,25 @@ function saveDataToSessionAndReload(param, value, saveUserParameter) {
     showWait();
     noDisconnect = true;
     quitConfirmed = true;
-    dojo.byId("directAccessPage").value = "parameter.php";
+    //gautier 3287
+    if(param == 'currentLocale'){
+      var currentItem=historyTable[historyPosition];
+      if(currentItem[2]=="object"){
+        var directAccessPage = "objectMain.php";
+        dojo.byId("changeCurrentLocale").value = "changeCurrentLocale";
+        dojo.byId("p1name").value = currentItem[0];
+        dojo.byId("p1value").value = currentItem[1];
+      }else{
+        var directAccessPage=getTargetFromCurrentScreen(currentItem[2]);
+      }
+      dojo.byId("directAccessPage").value = directAccessPage;
+    }else{
+      dojo.byId("directAccessPage").value = "parameter.php";
+      dojo.byId("p1name").value = "type";
+      dojo.byId("p1value").value = "userParameter";
+    }
     dojo.byId("menuActualStatus").value = menuActualStatus;
-    dojo.byId("p1name").value = "type";
-    dojo.byId("p1value").value = "userParameter";
-    dojo.byId("directAccessForm").submit(); 
+    dojo.byId("directAccessForm").submit();
   };
   saveDataToSession(param, value, saveUserParameter, callBack);
 }
