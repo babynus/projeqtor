7103dae9098caab64875e9ef63bb0eb08232c820
florent <florent.arnaud@projeqtor.net>
2020-03-24 17:04:01 +0000

    FAR ticket 4280

---
+++ projeqtor/db/projeqtor_V8.5.0.sql
@@ -0,0 +1,17 @@
+-- ///////////////////////////////////////////////////////////
+-- // PROJECTOR                                             //
+-- //-------------------------------------------------------//
+-- // Version : 8.5.0                                       //
+-- // Date : 2020-03-23                                     //
+-- ///////////////////////////////////////////////////////////
+-- Patch on V8.4.0
+
+
+INSERT INTO `${prefix}habilitationother` (idProfile,scope,rightAccess) VALUES 
+(1,'validatePlanning','1'),
+(2,'validatePlanning','1'),
+(3,'validatePlanning','1'),
+(4,'validatePlanning','2'),
+(6,'validatePlanning','2'),
+(7,'validatePlanning','2'),
+(5,'validatePlanning','2');
