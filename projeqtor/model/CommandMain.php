990fcaff230854e3ff4117cc3ea58104699734df
damian <damian.sanchez@projeqtor.net>
2020-04-02 15:34:11 +0000

    DSAN

--- projeqtor/model/CommandMain.php
+++ projeqtor/model/CommandMain.php
@@ -350,6 +350,20 @@ class CommandMain extends SqlElement {
 	    	$prj->updateValidatedWork();
     	}
     }
+    if($this->idSituation){
+      $old=$this->getOld();
+    	$situation = new Situation($this->idSituation);
+    	if($this->idProject != $situation->idProject){
+    		$critWhere = array('refType'=>get_class($this),'refId'=>$this->id);
+    		$situationList = $situation->getSqlElementsFromCriteria($critWhere,null,null);
+    		foreach ($situationList as $sit){
+    		  $sit->idProject = $this->idProject;
+    		  $sit->save();
+    		}
+    		ProjectSituation::updateSituation($this);
+    		ProjectSituation::updateSituation($old);
+    	}
+    }
     return $resultClass;
   }
   public function delete() {
