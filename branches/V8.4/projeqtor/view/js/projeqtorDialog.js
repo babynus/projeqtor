92269123577f099b9688ece4c7ef0a758b70547a
florent <florent@f9421885-1815-481d-89b5-6d9585689fe2>
2020-04-03 10:25:44 +0000

    FAR ticket 4584

--- branches/V8.4/projeqtor/view/js/projeqtorDialog.js
+++ branches/V8.4/projeqtor/view/js/projeqtorDialog.js
@@ -7054,6 +7054,19 @@ function redoItemButton() {
   //gautier
   } else if (currentScreen=="Planning" && currentItem[1]!=null){ 
     gotoElement(currentItem[0], currentItem[1], false, false, "planning");
+  }else if(currentScreen=='GanttSupplierContract' || currentScreen=='GanttClientContract'){
+    if(currentScreen=='GanttClientContract'){
+      loadMenuBarItem('GanttClientContract','menuGanttClientContract','bar');
+    }else{
+      loadMenuBarItem('GanttSupplierContract','menuGanttSupplierContract','bar');
+    }
+  }else if (currentScreen=='ContractGantt') {
+    dojo.byId('objectClass').value=currentItem[0];
+    loadContent('contractGanttMain.php?objectClass='+currentItem[0]+'&objectId='+currentItem[1], 'centerDiv');
+    dojo.byId('objectClass').value = currentItem[0];
+    dojo.byId('objectId').value = currentItem[1];
+    loadContent('objectDetail.php', 'detailDiv', 'listForm');
+    if (dijit.byId("detailRightDiv")) loadContent("objectStream.php", "detailRightDiv", "listForm"); 
   } else {
     target=getTargetFromCurrentScreen(currentScreen);
     loadContent(target,"centerDiv"); 
