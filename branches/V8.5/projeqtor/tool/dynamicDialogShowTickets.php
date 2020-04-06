fae6db0a8641306f21a454112022f40579ca1204
damian <damian@f9421885-1815-481d-89b5-6d9585689fe2>
2020-04-06 16:08:16 +0000

    DSAN

--- branches/V8.5/projeqtor/tool/dynamicDialogShowTickets.php
+++ branches/V8.5/projeqtor/tool/dynamicDialogShowTickets.php
@@ -51,7 +51,10 @@
   echo '<table style="width:100%;">';
   echo '<tr>';
   echo '<td class="linkHeader" style="width:15%">' . i18n('colElement') . '</td>';
-  echo '<td class="linkHeader" style="width:65%">' . i18n('colName') . '</td>';
+  echo '<td class="linkHeader" style="width:35%">' . i18n('colName') . '</td>';
+  echo '<td class="linkHeader" style="width:10%">' . i18n('colEstimated') . '</td>';
+  echo '<td class="linkHeader" style="width:10%">' . i18n('colReal') . '</td>';
+  echo '<td class="linkHeader" style="width:10%">' . i18n('colLeft') . '</td>';
   echo '<td class="linkHeader" style="width:20%">' . i18n('colIdStatus') . '</td>';
   echo '</tr>';
   foreach ( $list as $ticket ) {
@@ -68,54 +71,18 @@
     if ($canGoto) {
       $goto=' onClick="dijit.byId(\'dialogShowTickets\').hide();gotoElement(' . "'" . $class . "','" . htmlEncode($ticket->id) . "'" . ');" style="cursor: pointer;" ';
     }
-    echo '<td class="linkData" ' . $goto . ' style="position:relative;width:65%">';
+    echo '<td class="linkData" ' . $goto . ' style="position:relative;width:35%">';
     echo htmlEncode($ticket->name);
     echo formatUserThumb($userId, $userName, 'Creator');
     echo formatDateThumb($ticket->creationDateTime, null);
     echo '</td>';
+    $workElement = $ticket->WorkElement;
+    echo '<td class="linkData"  style="width:10%;text-align:center;">' . Work::displayWorkWithUnit($workElement->plannedWork) . '</td>';
+    echo '<td class="linkData"  style="width:10%;text-align:center;">' . Work::displayWorkWithUnit($workElement->realWork) . '</td>';
+    echo '<td class="linkData"  style="width:10%;text-align:center;">' . Work::displayWorkWithUnit($workElement->leftWork) . '</td>';
     $objStatus=new Status($ticket->idStatus);
-    echo '<td class="dependencyData colorNameData"  style="width:15%">' . colorNameFormatter($objStatus->name . "#split#" . $objStatus->color) . '</td>';
+    echo '<td class="dependencyData colorNameData"  style="width:20%">' . colorNameFormatter($objStatus->name . "#split#" . $objStatus->color) . '</td>';
     echo '</tr>';
   }
   echo '</table>';
-  if($obj->isPlanningActivity){
-    $planningElement = SqlElement::getSingleSqlElementFromCriteria('ActivityPlanningElement', array('refType'=>$refType, 'refId'=>$refId));
-  	echo '<table class="detail" style="margin-top:10px;">';
-  	echo '<tr class="detail">';
-  	echo '<td class="detail"></td>';
-  	echo '<td class="detail" style="min-width:75px;">';
-  	echo '<div class="tabLabel" style="text-align:left;white-space:nowrap;">'.i18n('colWorkElementCount').'</div>';
-  	echo '</td>';
-  	echo '<td class="detail" style="min-width:75px;">';
-  	echo '<div class="tabLabel" style="text-align:left;white-space:nowrap;">'.i18n('colEstimated').'</div>';
-  	echo '</td>';
-  	echo '<td class="detail" style="min-width:75px;">';
-  	echo '<div class="tabLabel" style="text-align:left;white-space:nowrap;">'.i18n('colReal').'</div>';
-  	echo '</td>';
-  	echo '<td class="detail" style="min-width:75px;">';
-  	echo '<div class="tabLabel" style="text-align:left;white-space:nowrap;">'.i18n('colLeft').'</div>';
-  	echo '</td>';
-  	echo '</tr>';
-  	echo '<tr class="detail">';
-  	echo '<td class="smallLabel" style="text-align:right;width:145px;">';
-  	echo '<label class="label smallLabel">'.i18n('colTicket').' : </label>';
-  	echo '</td>';
-  	echo '<td style="width:90%;white-space:nowrap;">';
-  	echo '<div class="display generalColClass inputworkElementCountClass">'.count($list).'</div>';
-  	echo '</td>';
-  	echo '<td class="detail" style="text-align:left;white-space:nowrap;">';
-  	echo '<div disabled dojoType="dijit.form.NumberTextBox" value="'.$planningElement->workElementEstimatedWork.'" class="input dijitNumberTextBox dijitNumberTextBoxReadOnly dijitTextBoxReadOnly generalColClass workElementEstimatedWorkClass" style="width: 72px"></div>';
-  	echo '<span class="generalColClass workElementEstimatedWorkClass" style="position:relative;top:2px">'.Work::displayShortWorkUnit().'</span>';
-  	echo '</td>';
-  	echo '<td class="detail" style="text-align:left;white-space:nowrap;">';
-  	echo '<div disabled dojoType="dijit.form.NumberTextBox" value="'.$planningElement->workElementRealWork.'" class="input dijitNumberTextBox dijitNumberTextBoxReadOnly dijitTextBoxReadOnly generalColClass workElementEstimatedWorkClass" style="width: 72px"></div>';
-  	echo '<span class="generalColClass workElementRealWorkClass" style="position:relative;top:2px">'.Work::displayShortWorkUnit().'</span>';
-  	echo '</td>';
-  	echo '<td class="detail" style="text-align:left;white-space:nowrap;">';
-  	echo '<div disabled dojoType="dijit.form.NumberTextBox" value="'.$planningElement->workElementLeftWork.'" class="input dijitNumberTextBox dijitNumberTextBoxReadOnly dijitTextBoxReadOnly generalColClass workElementEstimatedWorkClass" style="width: 72px"></div>';
-  	echo '<span class="generalColClass workElementLeftWorkClass" style="position:relative;top:2px">'.Work::displayShortWorkUnit().'</span>';
-  	echo '</td>';
-  	echo '</tr>';
-  	echo '</table>';
-  }
 ?>
