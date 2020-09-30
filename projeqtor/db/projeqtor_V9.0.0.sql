ae2997a08a64454d83975e1a7fd2ac129380fdd5
florent <florent.arnaud@projeqtor.net>
2020-09-30 13:03:35 +0000

    FAR

--- projeqtor/db/projeqtor_V9.0.0.sql
+++ projeqtor/db/projeqtor_V9.0.0.sql
@@ -13,5 +13,39 @@ CREATE TABLE `${prefix}navigation` (
   PRIMARY KEY (`id`)
 ) ENGINE=innoDB DEFAULT CHARSET=utf8 ;
 
-
+INSERT INTO `${prefix}navigation` (`name`, `idParent`, `idMenu`) VALUES
+('navPlanning','',''),
+('navTicketing','',''),
+('navFolowUp','',''),
+('navRepports','',''),
+('navAdministration','',''),
+('navParamters',5,''),
+('navOther',1,''),
+('navRepports',2,''),
+('navRepportsWork',3,''),
+('navWork',9,''),
+('navWorkDetailed',9,''),
+('navWorkIndividual',9,''),
+('menuProject',1,16),
+('menuActivity',1,25),
+('menuMilestone',1,26),
+('menuPlanning',1,9),
+('menuPortfolioPlanning',1,123),
+('menuTicket',2,22),
+('menuTicketSimple',2,118),
+('menuTicketType',2,53),
+('menuTicketDelay',2,89),
+('menuTicketDelayPerProject',2,182),
+('menuDashboardTicket',2,150),
+('menuMilestone',2,26),
+('menuImputation',3,8),
+('menuAbsence',3,203),
+('menuDiary',3,133),
+('menuImputationValidation',3,204),
+('menuConsultationPlannedWorkManual',3,9),
+('menuImputation',3,8),
+('menuAbsence',3,203),
+('menuDiary',3,133),
+('menuGlobalParameter',6,18),
+('menuUserParameter',6,19);
 
