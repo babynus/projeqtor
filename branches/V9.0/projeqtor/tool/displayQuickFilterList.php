e7b92fe92994e857654a1c5c3f2461056c85c47b
gautier <gautier.pradines@projeqtor.net>
2020-10-23 07:32:30 +0000

    GPA #filter

--- branches/V9.0/projeqtor/tool/displayQuickFilterList.php
+++ branches/V9.0/projeqtor/tool/displayQuickFilterList.php
@@ -51,17 +51,32 @@ $comboDetail=false;
 if (RequestHandler::isCodeSet('comboDetail')) {
   $comboDetail=true;
 }
-//dijit.byId('listIdFilterQuickMenu').domNode.style.display = 'block';
 ?>
 <table style="width:99%;" id="quickFilterList">
-  <tr style="width:100%;border-bottom:solid 1px;text-transform: uppercase;"><td><?php echo i18n("filters");?></td></tr>
+  <tr style="width:100%;border-bottom:solid 1px;">
+  <td><table><tr> 
+    <td style="width:89%;text-transform: uppercase;"><?php echo i18n("filters");?></td>
+    <td width="6px " class="allSearchTD resetSearchTD allSearchFixLength">
+      <button dojoType="dijit.form.Button" type="button" >
+          <?php echo i18n('buttonReset');?>
+          <?php $listStatus = $object->getExistingStatus(); $lstStat=(count($listStatus));?>
+            <script type="dojo/method" event="onClick">
+                     var lstStat = <?php echo json_encode($lstStat); ?>;
+                     resetFilterQuick(lstStat);
+             </script>
+      </button>
+    </td> 
+   </tr></table></td>
+  </tr>
   <tr><td>
     <table style="width:100%">
      <br/> 
-      <tr><td style="width:95%"></td><td style="width:5%"><?php echo i18n("alwaysDisplay");?></td></tr>
+      <tr><td style="width:25%"></td><td style="width:65%"></td><td style="width:10%;font-sytle:italic;color:grey;"><?php echo i18n("alwaysDisplay");?></td></tr>
       <tr>
-        <td style="width:95%">
-          <span class="nobr">&nbsp;&nbsp;&nbsp;&nbsp; <?php echo i18n("colId");?>&nbsp;</span> 
+        <td style="text-align:right;width:25%">
+          <span class="nobr"><?php echo i18n("colId")?>&nbsp;:&nbsp;</span> 
+        </td>
+        <td style="width:65%">
           <div title="<?php echo i18n('filterOnId')?>" style="width:<?php echo $referenceWidth;?>px" class="filterField rounded" dojoType="dijit.form.TextBox" 
                 type="text" id="listIdFilterQuick" name="listIdFilterQuick" value="<?php if(!$comboDetail and sessionValueExists('listIdFilter'.$objectClass)){ echo getSessionValue('listIdFilter'.$objectClass); }?>">
             <script type="dojo/method" event="onKeyUp" >
@@ -80,7 +95,7 @@ if (RequestHandler::isCodeSet('comboDetail')) {
             </script>
           </div>
         </td>
-        <td style="width:5%">
+        <td style="width:10%;text-align:center;">
           <div  id="listIdFilterQuickSw" name="listIdFilterQuickSw" class="colorSwitch" data-dojo-type="dojox/mobile/Switch" value="<?php if(sessionValueExists('listIdFilterQuickSw'.$objectClass)){ echo getSessionValue('listIdFilterQuickSw'.$objectClass); }else{?>off<?php }?>" leftLabel="" rightLabel="">
             <script type="dojo/method" event="onStateChanged" >
               saveDataToSession('listIdFilterQuickSw<?php echo $objectClass;?>',this.value);
@@ -102,11 +117,14 @@ if (RequestHandler::isCodeSet('comboDetail')) {
           </div>
         </td>
       </tr>
-        <?php if ( property_exists($obj,'name') or get_class($obj)=='Affectation') { ?>
+      
+      <?php if ( property_exists($obj,'name') or get_class($obj)=='Affectation') { ?>
       <tr> 
-        <td style="width:95%">
-        <span class="nobr"><?php echo i18n("colName");?>&nbsp;</span> 
-          <div title="<?php echo i18n('filterOnName')?>" style="width:<?php echo $referenceWidth*2;?>px" type="text" class="filterField rounded" dojoType="dijit.form.TextBox" 
+        <td style="text-align:right;width:25%">
+          <span class="nobr"><?php echo i18n("colName");?>&nbsp;:&nbsp;</span> 
+        </td>
+        <td style="width:65%">
+          <div title="<?php echo i18n('filterOnName')?>" style="width:<?php echo $referenceWidth*4;?>px" type="text" class="filterField rounded" dojoType="dijit.form.TextBox" 
               id="listNameFilterQuick" name="listNameFilterQuick"  value="<?php if(!$comboDetail and sessionValueExists('listNameFilter'.$objectClass)){ echo getSessionValue('listNameFilter'.$objectClass); }?>">
             <script type="dojo/method" event="onKeyUp" >
              if(dijit.byId('listNameFilterQuick').get('value') =='' && dijit.byId('listNameFilterQuickSw').get('value')=='off'){
@@ -124,7 +142,7 @@ if (RequestHandler::isCodeSet('comboDetail')) {
             </script>
           </div>
         </td>
-        <td style="width:5%">
+        <td style="width:10%;text-align:center;">
           <div id="listNameFilterQuickSw" name="listNameFilterQuickSw" class="colorSwitch" data-dojo-type="dojox/mobile/Switch" value="<?php if(sessionValueExists('listNameFilterQuickSw'.$objectClass)){ echo getSessionValue('listNameFilterQuickSw'.$objectClass); }else{?>off<?php }?>" leftLabel="" rightLabel="">
             <script type="dojo/method" event="onStateChanged" >
               saveDataToSession('listNameFilterQuickSw<?php echo $objectClass;?>',this.value);
@@ -149,12 +167,13 @@ if (RequestHandler::isCodeSet('comboDetail')) {
       
       <?php if ( ( property_exists($obj,'id' . $objectClass . 'Type')) or ( $objectClass=='EmployeeLeaveEarned' and property_exists($obj,'idLeaveType')) ) { ?>
       <tr>
-        <td style="width:95%"> <span class="nobr">&nbsp;<?php echo i18n("colType");?> &nbsp;</span>
+        <td style="text-align:right;width:25%;text-transform:lowercase;"> <span class="nobr"><?php echo i18n("colType");?> &nbsp;:&nbsp;</span> </td>
+        <td style="width:65%">
          <select dojoType="dijit.form.FilteringSelect" class="input"  id="listTypeFilterQuick" name="listTypeFilterQuick"
           <?php echo autoOpenFilteringSelect();?>
             title="<?php echo i18n('helpLang');?>" style="width:<?php echo $referenceWidth*4;?>px" value="<?php if(!$comboDetail and sessionValueExists('listTypeFilter'.$objectClass)){ echo getSessionValue('listTypeFilter'.$objectClass); }?>">
             <script type="dojo/connect" event="onChange" >
-              if(this.value ==' ' && dijit.byId('listTypeFilterQuickSw').get('value')=='off'){
+              if( this.value ==' ' && dijit.byId('listTypeFilterQuickSw').get('value')=='off'){
                 dojo.byId('listTypeFilterSpan').style.display="none";
                 dijit.byId('listTypeFilter').domNode.style.display = 'none';
               }else{
@@ -170,7 +189,7 @@ if (RequestHandler::isCodeSet('comboDetail')) {
             <?php  htmlDrawOptionForReference($idClassType, $objectType, $obj, false); ?>
           </select>
         </td>
-        <td style="width:5%">
+        <td style="width:10%;text-align:center;">
           <div id="listTypeFilterQuickSw" name="listTypeFilterQuickSw" class="colorSwitch" data-dojo-type="dojox/mobile/Switch" value="<?php if(sessionValueExists('listTypeFilterQuickSw'.$objectClass)){ echo getSessionValue('listTypeFilterQuickSw'.$objectClass); }else{?>off<?php }?>" leftLabel="" rightLabel="">
             <script type="dojo/method" event="onStateChanged" >
               saveDataToSession('listTypeFilterQuickSw<?php echo $objectClass;?>',this.value);
@@ -183,7 +202,7 @@ if (RequestHandler::isCodeSet('comboDetail')) {
                     dijit.byId('listTypeFilter').domNode.style.display = 'block';
                 }
               }else{
-                if(dojo.byId('listTypeFilterSpan').style.display=="block" && dijit.byId('listTypeFilter').get('value')=='') {
+                if(dojo.byId('listTypeFilterSpan').style.display=="block" && ( dijit.byId('listTypeFilter').get('value')=='' || dijit.byId('listTypeFilter').get('value')==' ' )) {
                   dojo.byId('listTypeFilterSpan').style.display="none";
                   dijit.byId('listTypeFilter').domNode.style.display = 'none';
                 }
@@ -197,13 +216,14 @@ if (RequestHandler::isCodeSet('comboDetail')) {
       
       <?php if ( property_exists($obj,'idClient') ) { ?>
       <tr>
-        <td style="width:95%"><span class="nobr">&nbsp; <?php echo i18n("colClient");?>&nbsp;</span>
-                <select title="<?php echo i18n('filterOnClient')?>" type="text" class="filterField roundedLeft" dojoType="dijit.form.FilteringSelect"
-                  <?php echo autoOpenFilteringSelect();?> 
-                  data-dojo-props="queryExpr: '*${0}*',autoComplete:false"
-                  id="listClientFilterQuick" name="listClientFilterQuick" style="width:<?php echo $referenceWidth*4;?>px" value="<?php if(!$comboDetail and sessionValueExists('listClientFilter'.$objectClass)){ echo getSessionValue('listClientFilter'.$objectClass); }?>" >
-                  <?php htmlDrawOptionForReference('idClient', $objectClient, $obj, false); ?>
-                  <script type="dojo/method" event="onChange" >
+        <td style="text-align:right;width:25%;text-transform:lowercase;"><span class="nobr">&nbsp; <?php echo i18n("colClient");?>&nbsp;:&nbsp;</span> </td>
+        <td style="width:65%">
+          <select title="<?php echo i18n('filterOnClient')?>" type="text" class="filterField roundedLeft" dojoType="dijit.form.FilteringSelect"
+            <?php echo autoOpenFilteringSelect();?> 
+            data-dojo-props="queryExpr: '*${0}*',autoComplete:false"
+            id="listClientFilterQuick" name="listClientFilterQuick" style="width:<?php echo $referenceWidth*4;?>px" value="<?php if(!$comboDetail and sessionValueExists('listClientFilter'.$objectClass)){ echo getSessionValue('listClientFilter'.$objectClass); }?>" >
+            <?php htmlDrawOptionForReference('idClient', $objectClient, $obj, false); ?>
+            <script type="dojo/method" event="onChange" >
                     if(this.value ==' ' && dijit.byId('listClientFilterQuickSw').get('value')=='off'){
                       dojo.byId('listClientFilterSpan').style.display="none";
                       dijit.byId('listClientFilter').domNode.style.display = 'none';
@@ -217,9 +237,9 @@ if (RequestHandler::isCodeSet('comboDetail')) {
                     dijit.byId('listClientFilter').set('value',this.value);
                     refreshJsonList('<?php echo $objectClass;?>');
                   </script>
-                </select>
+          </select>
         </td>
-        <td style="width:5%">
+        <td style="width:10%;text-align:center;">
           <div id="listClientFilterQuickSw" name="listClientFilterQuickSw" class="colorSwitch" data-dojo-type="dojox/mobile/Switch" value="<?php if(!$comboDetail and sessionValueExists('listClientFilterQuickSw'.$objectClass)){ echo getSessionValue('listClientFilterQuickSw'.$objectClass); }else{?>off<?php }?>" leftLabel="" rightLabel="">
             <script type="dojo/method" event="onStateChanged" >
               saveDataToSession('listClientFilterQuickSw<?php echo $objectClass;?>',this.value);
@@ -232,7 +252,7 @@ if (RequestHandler::isCodeSet('comboDetail')) {
                     dijit.byId('listClientFilter').domNode.style.display = 'block';
                 }
               }else{
-                if(dojo.byId('listClientFilterSpan').style.display=="block" && dijit.byId('listTypeFilter').get('value')=='') {
+                if(dojo.byId('listClientFilterSpan').style.display=="block" && ( dijit.byId('listTypeFilter').get('value')=='' || dijit.byId('listTypeFilter').get('value')==' ' )) {
                   dojo.byId('listClientFilterSpan').style.display="none";
                   dijit.byId('listClientFilter').domNode.style.display = 'none';
                 }
@@ -245,7 +265,8 @@ if (RequestHandler::isCodeSet('comboDetail')) {
       
       <?php if ( $objectClass == 'Budget' ) { ?>
         <tr>
-          <td style="width:95%"><span class="nobr">&nbsp; <?php echo i18n("colParentBudget");?>&nbsp;</span>
+          <td style="width:25%;text-align:right;text-transform:lowercase;"><span class="nobr">&nbsp; <?php echo i18n("colParentBudget");?>&nbsp;:&nbsp;</span></td>
+          <td style="width:65%;">
             <select title="<?php echo i18n('filterOnBudgetParent')?>" type="text" class="filterField roundedLeft" dojoType="dijit.form.FilteringSelect"
                 <?php echo autoOpenFilteringSelect();?> 
                 data-dojo-props="queryExpr: '*${0}*',autoComplete:false"
@@ -268,7 +289,7 @@ if (RequestHandler::isCodeSet('comboDetail')) {
                   </script>
           </select>
         </td>
-        <td style="width:5%">
+        <td style="width:10%;text-align:center;">
           <div id="listBudgetParentFilterQuickSw" name="listBudgetParentFilterQuickSw" class="colorSwitch" data-dojo-type="dojox/mobile/Switch" value="<?php if(!$comboDetail and sessionValueExists('listBudgetParentFilterQuickSw'.$objectClass)){ echo getSessionValue('listBudgetParentFilterQuickSw'.$objectClass); }else{?>off<?php }?>" leftLabel="" rightLabel="">
             <script type="dojo/method" event="onStateChanged" >
               saveDataToSession('listClientFilterQuickSw<?php echo $objectClass;?>',this.value);
@@ -281,7 +302,7 @@ if (RequestHandler::isCodeSet('comboDetail')) {
                     dijit.byId('listBudgetParentFilter').domNode.style.display = 'block';
                 }
               }else{
-                if(dojo.byId('listBudgetParentFilterSpan').style.display=="block" && dijit.byId('listTypeFilter').get('value')=='') {
+                if(dojo.byId('listBudgetParentFilterSpan').style.display=="block" && (dijit.byId('listTypeFilter').get('value')=='' || dijit.byId('listTypeFilter').get('value')==' ' )){
                   dojo.byId('listBudgetParentFilterSpan').style.display="none";
                   dijit.byId('listBudgetParentFilter').domNode.style.display = 'none';
                 }
@@ -294,13 +315,13 @@ if (RequestHandler::isCodeSet('comboDetail')) {
       
       <?php  if (! $comboDetail) {?>
        <tr>
-         <td style="vertical-align: middle;">
-                 <span class="nobr">&nbsp;<?php echo i18n("quickSearch");?>&nbsp;</span> 
+         <td  style="width:25%;text-align:right;text-transform:lowercase;"><span class="nobr"><?php echo i18n("quickSearch");?>&nbsp;:&nbsp;</span></td>
+         <td style="width:65%;">
             <div title="<?php echo i18n('quickSearch')?>" type="text" class="filterField rounded" dojoType="dijit.form.TextBox" 
                id="quickSearchValueQuick" name="quickSearchValueQuick"
                style="width:200px;">
             </div>
-  	        <button title="<?php echo i18n('quickSearch')?>"  
+             <button title="<?php echo i18n('quickSearch')?>"  
   	          dojoType="dijit.form.Button" 
   	          id="listQuickSearchExecuteQuick" name="listQuickSearchExecuteQuick"
   	          iconClass="dijitButtonIcon dijitButtonIconSearch" class="detailButton" showLabel="false">
@@ -308,6 +329,8 @@ if (RequestHandler::isCodeSet('comboDetail')) {
                 quickSearchExecuteQuick();
               </script>
   	        </button>
+          </td>
+          <td style="width:10%">  
   	        <button title="<?php echo i18n('comboCloseButton')?>"  
                   dojoType="dijit.form.Button" 
             id="listQuickSearchCloseQuick" name="listQuickSearchCloseQuick"
@@ -315,52 +338,39 @@ if (RequestHandler::isCodeSet('comboDetail')) {
             <script type="dojo/connect" event="onClick" args="evt">
               quickSearchCloseQuick();
             </script>
-          </button>
-  	      </td>  
+          </button></td>  
 		    </tr>
 			  <?php } ?>
-      
-      <tr>
-          <td width="6px " class="allSearchTD resetSearchTD allSearchFixLength">
-            <button dojoType="dijit.form.Button" type="button" >
-                <?php echo i18n('buttonReset');?>
-                <?php $listStatus = $object->getExistingStatus(); $lstStat=(count($listStatus));?>
-                  <script type="dojo/method" event="onClick">
-                     var lstStat = <?php echo json_encode($lstStat); ?>;
-                     resetFilterQuick(lstStat);
-                  </script>
-              
-            </button>
-          </td>  
-      </tr>
+        
       <?php if ( property_exists($obj, 'idStatus') and Parameter::getGlobalParameter('filterByStatus') == 'YES' and $objectClass!='GlobalView') {  ?> 
       <tr>
-        <td width="36px" class="listButtonClass">
-            	<button title="<?php echo i18n('filterByStatus');?>"
-			             dojoType="dijit.form.Button"
-			             id="iconStatusButtonQuick" name="iconStatusButtonQuick"
-			             iconClass="dijitButtonIcon dijitButtonIconStatusChange" class="detailButton" showLabel="false">
-			             <script type="dojo/connect" event="onClick" args="evt">
-                     protectDblClick(this);
-						         if (dijit.byId('barFilterByStatus').domNode.style.display == 'none') {
+        <td  style="width:25%;text-align:right;text-transform:lowercase;"><span class="nobr"><?php echo i18n("filterByStatus");?>&nbsp;:&nbsp;</span></td>
+        <td></td>
+			  <td style="width:10%;text-align:center;">
+			   <div id="filterByStatusSwitch" name="filterByStatusSwitch" class="colorSwitch" data-dojo-type="dojox/mobile/Switch" value="<?php if(!$comboDetail and sessionValueExists('displayByStatusListSwitch'.$objectClass)){ echo getSessionValue('displayByStatusListSwitch'.$objectClass); }else{?>off<?php }?>" leftLabel="" rightLabel="">
+            <script type="dojo/method" event="onStateChanged" >
+              saveDataToSession('displayByStatusListSwitch<?php echo $objectClass;?>',this.value);
+              if (dijit.byId('barFilterByStatus').domNode.style.display == 'none') {
 							         dijit.byId('barFilterByStatus').domNode.style.display = 'block';
 						         } else {
 							         dijit.byId('barFilterByStatus').domNode.style.display = 'none';
 						         }
 						         dijit.byId('barFilterByStatus').getParent().resize();
                      saveDataToSession("displayByStatusList_<?php echo $objectClass;?>", dijit.byId('barFilterByStatus').domNode.style.display, true);
-          				 </script>
-			        </button>
-			 </td>
+            </script>
+          </div>
+			  </td>
       </tr>
       <?php } ?>
-      
+     </table>
+     <br><br>
+     <table style="width: 100%;">
       <tr style="border-top:solid 1px;">
         <td> 
           <button title="<?php echo i18n('advancedFilter')?>"  
                   dojoType="dijit.form.Button" 
                   id="listFilterFilterDisplay" name="listFilterFilterDisplay"
-                  iconClass="dijitButtonIcon dijitButtonIconPrint"
+                  iconClass="dijitButtonIcon dijitButtonIconFilter"
                    class="detailButton" showLabel="false">
                 <script type="dojo/connect" event="onClick" args="evt">
                    showFilterDialog();
@@ -368,7 +378,6 @@ if (RequestHandler::isCodeSet('comboDetail')) {
           </button>
           <?php echo i18n('advancedFilters');?>
         </td>
-        <td></td>
       </tr>  
     </table>
   </td></tr>
