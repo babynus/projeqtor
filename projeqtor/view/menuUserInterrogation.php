b019b740a1aa390855abbc2897fda43fba79f7a1
gautier <gautier.pradines@projeqtor.net>
2020-11-06 08:55:44 +0000

    GPA #interrogation

--- projeqtor/view/menuUserInterrogation.php
+++ projeqtor/view/menuUserInterrogation.php
@@ -38,7 +38,7 @@ $user=getSessionUser();
         title="<?php echo('reportBug'); ?>"
         style="text-align:left;margin-right:10px;margin-top:10px;height:23px;width:160px;
         onClick="showFilterDialog();">
-        <img src="css/customIcons/new/iconResolution.svg" style="-webkit-filter : hue-rotate(var(--image-hue-rotate)) saturate(var(--image-saturate)) brightness(var(--image-brightness))
+        <img src="css/customIcons/new/iconHelpBug.svg" style="-webkit-filter : hue-rotate(var(--image-hue-rotate)) saturate(var(--image-saturate)) brightness(var(--image-brightness))
              filter : hue-rotate(var(--image-hue-rotate)) saturate(var(--image-saturate)) brightness(var(--image-brightness));position:relative;left:5px;top:2px;background-repeat:no-repeat;width:20px;background-size:20px;"/>
          <div style="color:grey;position:relative;top:-19px;left:38px;"><?php echo i18n('reportBug'); ?></div>
           </div></td></tr></table>
@@ -57,7 +57,7 @@ $user=getSessionUser();
   </tr>
   <tr style="color:grey;height:15px;border-bottom:1px dotted;">
     <td style="cursor:pointer;">
-      <div   title="<?php echo i18n('keyboardShortcuts');?>" onClick="showHelp('ShortCut');">
+      <div  style="margin-top:6px;" title="<?php echo i18n('keyboardShortcuts');?>" onClick="showHelp('ShortCut');">
         <table style="width:100%">
           <tr>
             <td style="vertical-align:middle;">&nbsp;&nbsp;<?php echo i18n('keyboardShortcuts');?>&nbsp;&nbsp;</td>
@@ -68,7 +68,7 @@ $user=getSessionUser();
   </tr>
   <tr style="color:grey;height:15px;border-bottom:1px dotted;">
     <td style="cursor:pointer;">
-      <div  title="<?php echo i18n('aboutMessage');?>" onClick="showAbout(aboutMessage);">
+      <div style="margin-top:6px;" title="<?php echo i18n('aboutMessage');?>" onClick="showAbout(aboutMessage);">
         <table style="width:100%">
           <tr>
             <td style="vertical-align:middle;">&nbsp;&nbsp;<?php echo i18n('aboutMessage');?>&nbsp;&nbsp;</td>
@@ -77,10 +77,9 @@ $user=getSessionUser();
       </div>
     </td>
   </tr>
-  <tr>
-    <tr style="color:grey;height:15px;border-bottom:1px dotted;">
+  <tr style="color:grey;height:15px;border-bottom:1px dotted;">
     <td style="cursor:pointer;">
-      <div  title="<?php echo i18n('linkToPlugin');?>" onClick="showAbout(aboutMessage);">
+      <div style="margin-top:6px;" title="<?php echo i18n('linkToPlugin');?>" onClick="showAbout(aboutMessage);">
         <table style="width:100%">
           <tr>
             <td style="vertical-align:middle;">&nbsp;&nbsp;<?php echo i18n('linkToPlugin');?>&nbsp;&nbsp;</td>
@@ -92,7 +91,7 @@ $user=getSessionUser();
   <tr>
     <tr style="color:grey;height:15px;border-bottom:1px dotted;">
     <td style="cursor:pointer;">
-      <div  title="<?php echo i18n('linkToForum');?>" onClick="showAbout(aboutMessage);">
+      <div style="margin-top:6px;" title="<?php echo i18n('linkToForum');?>" onClick="showAbout(aboutMessage);">
         <table style="width:100%">
           <tr>
             <td style="vertical-align:middle;">&nbsp;&nbsp;<?php echo i18n('linkToForum');?>&nbsp;&nbsp;</td>
@@ -103,9 +102,9 @@ $user=getSessionUser();
   </tr>
   <tr>
     <td>
-  <table style="margin-top:15px;">
-    <tr>
-    <?php $urlGetNews = "http://projeqtor.org/admin/getNews.php";
+    <table style="margin-top:15px;">
+      <tr>
+      <?php $urlGetNews = "http://projeqtor.org/admin/getNews.php";
           $currentVersion=null;
          if (ini_get('allow_url_fopen')) {
            enableCatchErrors();
@@ -114,23 +113,31 @@ $user=getSessionUser();
          }
          $json = file_get_contents($urlGetNews);
          $obj = json_decode($json);
-         $i=0;
+         $i=1;
+         $userLang = getSessionValue('currentLocale');
+         $lang = "en";
+         if(substr($userLang,0,2)=="fr")$lang="fr";
          foreach ($obj as $objV=>$val){
             if($val!="id"){
               foreach ($val as $value){
-                if($i%2==0){?> <tr><?php } ?>
+                if($value->lang!=$lang)continue;
+                if($i%3==0){?> <tr><?php } ?>
               <td>
                 <table>
                   <tr>
-                    <td><div style="border-top-left-radius:5px;border-top-right-radius:5px;<?php if($i==0){?>margin-right:20px;<?php }?>width:150px;overflow:hidden;height:30px;background:<?php echo '#'.Parameter::getUserParameter('newGuiThemeColor');?>;" id="divMsg<?php echo $i;?>" name="divMsgFull<?php echo $i;?>" onClick="showMsg(divMsg<?php echo $i;?>,test;);"><?php echo $value->title;?></div></td>
+                    <td>
+                      <div style="overflow:hidden;margin-right:10px;margin-bottom:10px;height:60px;width:150px;background:#f5f5f5;border-radius:5px;" id="divMsgTitle<?php echo $i;?>" name="divMsgTitle<?php echo $i;?>" onClick="showMsg(<?php echo $i;?>,<?php echo $i/4;?>);">
+                        <?php echo $value->title;?>
+                      </div>
+                    </td>
                   </tr>
-                  <tr>
-                    <td><div style="border-bottom-left-radius:5px;border-bottom-right-radius:5px;margin-bottom:10px;width:150px;height:50px;overflow:hidden;background:#f2f5f5;" id="divMsgFull<?php echo $i;?>" name="divMsgFull<?php echo $i;?>"><?php echo $value->introtext;?></div></td>
+                   <tr>
+                    <td><div style="display:none;border-bottom-left-radius:5px;border-bottom-right-radius:5px;margin-bottom:10px;width:150px;height:50px;overflow-y:auto;background:#f2f5f5;" id="divMsgFull<?php echo $i;?>" name="divMsgFull<?php echo $i;?>" onClick="hideMsg(<?php echo $i;?>,<?php echo $i/4;?>);"><?php echo $value->introtext;?></div></td>
                   </tr>
                 </table>
               </td>
               <?php $i++;
-                if($i%2==0){?></tr><?php }
+                if($i%3==0){?></tr><?php }
               }
             }
          }
