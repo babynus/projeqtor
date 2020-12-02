316cc686d95cc84df3b983079a196cf303a5ee9b
florent <florent.arnaud@projeqtor.net>
2020-12-02 13:40:19 +0000

    FAR

--- branches/V9.0/projeqtor/db/projeqtor_V9.0.0.sql
+++ branches/V9.0/projeqtor/db/projeqtor_V9.0.0.sql
@@ -288,6 +288,6 @@ UPDATE `${prefix}habilitationother` set scope='canDeleteAttachment' where scope=
 
 ALTER TABLE `${prefix}type` add image varchar(200) DEFAULT NULL;
 
-UPDATE `${prefix}type` set image='view/css/customIcons/new/iconWarning.svg' where scope='Message' and name="ALERT";
-UPDATE `${prefix}type` set image='view/css/customIcons/new/iconAlert.svg' where scope='Message' and name="INFO";
-UPDATE `${prefix}type` set image='view/css/customIcons/new/iconInfo.svg' where scope='Message' and name="WARNING"; 	
+UPDATE `${prefix}type` set image='view/css/customIcons/new/iconAlert.svg' where scope='Message' and name="ALERT";
+UPDATE `${prefix}type` set image='view/css/customIcons/new/iconInfo.svg' where scope='Message' and name="INFO";
+UPDATE `${prefix}type` set image='view/css/customIcons/new/iconWarning.svg' where scope='Message' and name="WARNING"; 	
