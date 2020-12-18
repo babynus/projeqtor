57bcaa3c01ca7311658d7be897e1e689c1af2c4f
gautier <gautier@f9421885-1815-481d-89b5-6d9585689fe2>
2020-12-18 15:36:53 +0000

    GPA #oldgui

--- branches/V9.0/projeqtor/view/menuBar.php
+++ branches/V9.0/projeqtor/view/menuBar.php
@@ -402,7 +402,7 @@
       </div>
     </td>
     <?php } drawSeparator();?>
-      <td title="<?php ?>"  style="position:relative;width:55px;padding-left:5px">
+      <td title="<?php ?>"  style="position:relative;width:55px;<?php if(isNewGui()){?>padding-left:5px<?php }?>">
       <div dojoType="dijit.layout.ContentPane"  id="menuUserScreenTop" class="pseudoButton" style="position:relative;overflow:hidden;width:50px; height:28px; min-width:55px;top:-5px;">
         <div dojoType="dijit.form.DropDownButton"  title="<?php echo i18n("menuUserScreenTopTitle");?>" id="iconMenuUserScreen" style="display: table-cell;<?php if (!isNewGui()) {?>background-color: #D3D3D3;<?php }?>vertical-align: middle;position:relative;min-width:50px;top:-3px" >
 			    <table style="width:100%">
