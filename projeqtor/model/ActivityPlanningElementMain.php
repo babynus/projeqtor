988e1b3729b04b22299f65ce22f44be5cc599e3a
gautier <gautier.pradines@projeqtor.net>
2021-04-06 14:39:09 +0000

    GPA #activityWorkUnit

--- projeqtor/model/ActivityPlanningElementMain.php
+++ projeqtor/model/ActivityPlanningElementMain.php
@@ -632,12 +632,17 @@ class ActivityPlanningElementMain extends PlanningElement {
         echo '<div style="position:relative;"><div style="color:#AA0000;margin:0px 10px;text-align:center;position:absolute;top:-55px;height:60px;">'.i18n('colIsOnCriticalPath').'</div></div>';
       }
     }elseif ($item=='idWorkUnits'){
-      $paramEnableWorkUnit = Parameter::getGlobalParameter('enableWorkCommandManagement');
-      if($this->id and $paramEnableWorkUnit == 'true'){
-        $activityWU = new ActivityWorkUnit();
-        $listActWU = $activityWU->getSqlElementsFromCriteria(array('refId'=>$this->refId,'refType'=>'Activity'));
-        $obj = new Activity($this->refId);
-        drawActivityWorkUnit($listActWU,$obj);
+      if(Module::isModuleActive('moduleGestionCA') and $this->id){
+        $project = new Project($this->idProject);
+        if($project->ProjectPlanningElement->idRevenueMode == 2){
+          $paramEnableWorkUnit = Parameter::getGlobalParameter('enableWorkCommandManagement');
+          if( $paramEnableWorkUnit == 'true'){
+            $activityWU = new ActivityWorkUnit();
+            $listActWU = $activityWU->getSqlElementsFromCriteria(array('refId'=>$this->refId,'refType'=>'Activity'));
+            $obj = new Activity($this->refId);
+            drawActivityWorkUnit($listActWU,$obj);
+          }
+        }
       }
     }
   }
