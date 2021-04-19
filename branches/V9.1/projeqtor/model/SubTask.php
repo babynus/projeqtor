c5c846a83ebb57d4e380eb543dc0e200925d7fd8
florent <florent@f9421885-1815-481d-89b5-6d9585689fe2>
2021-04-19 14:31:46 +0000

    FAR

--- branches/V9.1/projeqtor/model/SubTask.php
+++ branches/V9.1/projeqtor/model/SubTask.php
@@ -97,7 +97,7 @@ class SubTask extends SqlElement {
     $critFld='idProject';
     $critVal=$obj->idProject;
     $widthDisplay=(RequestHandler::isCodeSet("destinationWidth")?RequestHandler::getValue("destinationWidth"):"");
-    if(!$gloablView){
+    if(!$gloablView and !$print){
       $priority=new Priority();
       $allPrio=$priority->getSqlElementsFromCriteria(null,null,"1=1");
       foreach ($allPrio as $id=>$priority){
@@ -129,13 +129,21 @@ class SubTask extends SqlElement {
       echo' </div>';
       echo'</div>';
     }
+    if(!$print){
+      if (!$refresh ) echo '<tr><td colspan="4"><div id="'.$refType.'_'.$refId.'_drawSubTask" dojotype="dijit.layout.ContentPane" region="center">';
+      echo '<table style="width:100%;margin-top: 10px;" dojotype="dojo.dnd.Source" dndType="subTask_'.$refType.'_'.$refId.'" withhandles="true" 
+                    id="dndSubTask_'.$refType.'_'.$refId.'" jsId="dndSubTask_'.$refType.'_'.$refId.'" data-dojo-id="test">';
+    }else {
+      echo '<tr><td colspan="2" style="width:100%;">';
+      echo '<table style="width:100%;">';
+    }
+    if($gloablView and !$print ){
+      echo      '<input id="SubTaskIdResourceFilter_'.$refType.'_'.$refId.'" value="'.$idResource.'" type="hidden" />';
+    }
     
-    if (!$refresh) echo '<tr><td colspan="4"><div id="'.$refType.'_'.$refId.'_drawSubTask" dojotype="dijit.layout.ContentPane">';
-    echo '<table style="width:100%;margin-top: 10px;" dojotype="dojo.dnd.Source" dndType="subTask_'.$refType.'_'.$refId.'" withhandles="true" id="dndSubTask_'.$refType.'_'.$refId.'" jsId="dndSubTask_'.$refType.'_'.$refId.'" data-dojo-id="test">';
-    if($gloablView )echo      '<input id="SubTaskIdResourceFilter_'.$refType.'_'.$refId.'" value="'.$idResource.'" type="hidden" />';
     echo      '<input id="subTaskView" value="'.$view.'" type="hidden" />';
     echo  '<tr style="width:100%">';
-    echo    '<td class="linkHeader" style="width:2%"></td>';
+    if(!$print)echo    '<td class="linkHeader" style="width:2%"></td>';
     echo    '<td class="linkHeader" style="'.(($gloablView and $widthDisplay>='1530')?'width:64%;':'width:52%;').'">'.i18n('colName').'</td>';
     echo    '<td class="linkHeader" style="'.(($gloablView and $widthDisplay>='1530')?'width:12%;':'width:18%;').'">'.i18n('colPriority').'</td>';
     echo    '<td class="linkHeader" style="'.(($gloablView and $widthDisplay>='1530')?'width:12%;':'width:18%;').'">'.i18n('colResponsible').'</td>';
@@ -145,56 +153,82 @@ class SubTask extends SqlElement {
       foreach ($res as $id=>$subTask){
         $prioSubTask=new Priority($subTask->idPriority);
         $colorPrio=$prioSubTask->color;
-        echo  '<tr  id="'.$refType.'_'.$refId.'_subTaskRow_'.$subTask->id.'" '.(($rightUpdate=='NO' and $rightRead=='YES')?'':'class="dojoDndItem" dndType="subTask_'.$refType.'_'.$refId.'"').'  >';
-        echo      '<input id="sortOrder_'.$refType.'_'.$refId.'_'.$subTask->id.'" value="'.$subTask->sortOrder.'" type="hidden" />';
-        if($rightUpdate=='NO' and $rightRead=='YES'){
-          echo   '<td class="todoListTab" id="'.$refType.'_'.$refId.'_grabDive_0" >&nbsp;</td>';
+        if(!$print){
+          echo  '<tr  id="'.$refType.'_'.$refId.'_subTaskRow_'.$subTask->id.'" '.(($rightUpdate=='NO' and $rightRead=='YES')?'':'class="dojoDndItem" dndType="subTask_'.$refType.'_'.$refId.'"').'  >';
+          echo      '<input id="sortOrder_'.$refType.'_'.$refId.'_'.$subTask->id.'" value="'.$subTask->sortOrder.'" type="hidden" />';
+          if($rightUpdate=='NO' and $rightRead=='YES'){
+            echo   '<td class="todoListTab" id="'.$refType.'_'.$refId.'_grabDive_0" >&nbsp;</td>';
+          }else{
+            echo    '<td  class="dojoDndHandle handleCursor todoListTab"  style="text-align: center;"><img style="width:7px;top:4px;position: relative;" src="css/images/iconDrag.gif"></td>';
+          }
+          
+          echo    '<td class="todoListTab" style="white-space:nowrap;width:auto;margin-right:5px;text-align: center;" >';
+          echo      '<div title="'.i18n('colName').'"  type="text"  id="'.$refType.'_'.$refId.'_nameNewSubTask_'.$subTask->id.'" dojoType="dijit.form.TextBox" style="'.(($gloablView)?"width:98%;":"width:90%;" ).'" value="'. htmlEncode($subTask->name).'" ';
+          if ($rightUpdate=='NO' and $rightRead=='YES'){
+            echo ' readonly="true">';
+          }else {
+            echo 'onChange="updateSubTask('.$subTask->id.',\''.$refType.'\','.$refId.');"  >';
+          }
+          echo    '</div></td>';
+          echo    '<td class="todoListTab" style="white-space:nowrap;text-align: center;background-color:'.$colorPrio.';">';
+          echo      '<select dojoType="dijit.form.FilteringSelect"  id="'.$refType.'_'.$refId.'_priorityNewSubTask_'.$subTask->id.'" name="'.$refType.'_'.$refId.'_priorityNewSubTask_'.$subTask->id.'" style="width:auto;" class="input" '.autoOpenFilteringSelect().''; 
+          if ($rightUpdate=='NO' and $rightRead=='YES'){
+            echo ' readonly="true">';
+          }else {
+            echo 'onChange="updateSubTask('.$subTask->id.',\''.$refType.'\','.$refId.',\'true\',\'false\');"  >';
+          }
+                      htmlDrawOptionForReference('idPriority',$subTask->idPriority);
+          echo      '</select>';
+          echo    '</td>';
+          echo    '<td class="todoListTab" style="white-space:nowrap;text-align: center;">';
+          echo      '<select dojoType="dijit.form.FilteringSelect" id="'.$refType.'_'.$refId.'_resourceNewSubTask_'.$subTask->id.'" name="'.$refType.'_'.$refId.'_resourceNewSubTask_'.$subTask->id.'" style="width:auto;" class="input"² '.autoOpenFilteringSelect().'';
+          if ($rightUpdate=='NO' and $rightRead=='YES'){
+            echo ' readonly="true" >';
+          }else {
+            echo 'onChange="updateSubTask('.$subTask->id.',\''.$refType.'\','.$refId.',\'false\',\'true\');">';
+          }
+                      htmlDrawOptionForReference('idResource',$subTask->idResource,$obj,false,$critFld,$critVal);
+          echo      '</select>';
+          echo    '</td>';
+          echo    '<td   style="white-space:nowrap;text-align: center;border: 1px solid #AAAAAA;">';
+                    $subTask->drawStatusSubTask($subTask->id,$subTask->done,$subTask->idle,$subTask->handled,$refType,$refId,$gloablView,$rightUpdate,$rightRead);
+          echo    '</td>';
+          echo  '</tr>';
+          $lastSortRegist=$subTask->sortOrder;
         }else{
-          echo    '<td  class="dojoDndHandle handleCursor todoListTab"  style="text-align: center;"><img style="width:7px;top:4px;position: relative;" src="css/images/iconDrag.gif"></td>';
+          $resource= new Resource();
+          $namePrio=($subTask->idPriority!='' or $subTask->idPriority!=0 )?SqlList::getNameFromId(get_class($prioSubTask), $subTask->idPriority):'';
+          echo($subTask->idPriority);
+          echo($namePrio);
+          $nameResource=($subTask->idResource!='' or $subTask->idResource!=0)?SqlList::getNameFromId(get_class($resource), $subTask->idResource):'';
+          $backgroundColor="";
+          $i18n=' ';
+          if($subTask->handled==1){
+            $backgroundColor="background-color:#FACA77;";
+            $i18n=i18n('colHandled');
+          }else if($subTask->done==1){
+            $backgroundColor="background-color:#57CE44;";
+            $i18n=i18n('done');
+          }else if($subTask->idle==1){
+            $backgroundColor="background-color:#B7B3A9;";
+            $i18n=i18n('colIdle');
+          }
+          echo  '<tr>';
+          echo   '<td class="assignData" >'.htmlEncode($subTask->name).'</td>';
+          echo    '<td class="assignData" style="white-space:nowrap;text-align: center;background-color:'.$colorPrio.';" >'.htmlEncode($namePrio).'</td>';
+          echo    '<td class="assignData"   style="white-space:nowrap;text-align: center;">'.htmlEncode($nameResource).'</td>';
+          echo    '<td class="assignData" style="white-space:nowrap;text-align: center;'.$backgroundColor.'">'.htmlEncode($i18n).'</td>';
+          echo  '</tr>';
         }
-        
-        echo    '<td class="todoListTab" style="white-space:nowrap;width:auto;margin-right:5px;text-align: center;" >';
-        echo      '<div title="'.i18n('colName').'"  type="text"  id="'.$refType.'_'.$refId.'_nameNewSubTask_'.$subTask->id.'" dojoType="dijit.form.TextBox" style="'.(($gloablView)?"width:98%;":"width:90%;" ).'" value="'. htmlEncode($subTask->name).'" ';
-        if ($rightUpdate=='NO' and $rightRead=='YES'){
-          echo ' readonly="true">';
-        }else {
-          echo 'onChange="updateSubTask('.$subTask->id.',\''.$refType.'\','.$refId.');"  >';
-        }
-        echo    '</td>';
-        echo    '<td class="todoListTab" style="white-space:nowrap;text-align: center;background-color:'.$colorPrio.';">';
-        echo      '<select dojoType="dijit.form.FilteringSelect"  id="'.$refType.'_'.$refId.'_priorityNewSubTask_'.$subTask->id.'" name="'.$refType.'_'.$refId.'_priorityNewSubTask_'.$subTask->id.'" style="width:auto;" class="input" '.autoOpenFilteringSelect().''; 
-        if ($rightUpdate=='NO' and $rightRead=='YES'){
-          echo ' readonly="true">';
-        }else {
-          echo 'onChange="updateSubTask('.$subTask->id.',\''.$refType.'\','.$refId.',\'true\',\'false\');"  >';
-        }
-                    htmlDrawOptionForReference('idPriority',$subTask->idPriority);
-        echo      '</select>';
-        echo    '</td>';
-        echo    '<td class="todoListTab" style="white-space:nowrap;text-align: center;">';
-        echo      '<select dojoType="dijit.form.FilteringSelect" id="'.$refType.'_'.$refId.'_resourceNewSubTask_'.$subTask->id.'" name="'.$refType.'_'.$refId.'_resourceNewSubTask_'.$subTask->id.'" style="width:auto;" class="input"² '.autoOpenFilteringSelect().'';
-        if ($rightUpdate=='NO' and $rightRead=='YES'){
-          echo ' readonly="true" >';
-        }else {
-          echo 'onChange="updateSubTask('.$subTask->id.',\''.$refType.'\','.$refId.',\'false\',\'true\');">';
-        }
-                    htmlDrawOptionForReference('idResource',$subTask->idResource,$obj,false,$critFld,$critVal);
-        echo      '</select>';
-        echo    '</td>';
-        echo    '<td   style="white-space:nowrap;text-align: center;border: 1px solid #AAAAAA;">';
-                  $subTask->drawStatusSubTask($subTask->id,$subTask->done,$subTask->idle,$subTask->handled,$refType,$refId,$gloablView,$rightUpdate,$rightRead);
-        echo    '</td>';
-        echo  '</tr>';
-        $lastSortRegist=$subTask->sortOrder;
       }
     }
-    if($rightUpdate=='YES'){
+    if($rightUpdate=='YES' and !$print){
       $lastSort=(!empty($res))? $lastSortRegist :0;
       echo  '<tr id="'.$refType.'_'.$refId.'_newSubTaskRow" >';
       echo      '<input id="sortOrder_'.$refType.'_'.$refId.'_0" value="'.$lastSort.'" type="hidden" />';
        echo   '<td class="todoListTab" id="'.$refType.'_'.$refId.'_grabDive_0" >&nbsp;</td>';
       echo    '<td class="todoListTab" style="white-space:nowrap;text-align: center;">';
-      echo      '<div title="'.i18n('colName').'"  type="text"  id="'.$refType.'_'.$refId.'_nameNewSubTask_0" dojoType="dijit.form.TextBox" style="'.(($gloablView)?"width:98%;":"width:90%;" ).'" onChange="updateSubTask(0,\''.$refType.'\','.$refId.');" value="">';
+      echo      '<div title="'.i18n('colName').'"  type="text"  id="'.$refType.'_'.$refId.'_nameNewSubTask_0" dojoType="dijit.form.TextBox" style="'.(($gloablView)?"width:98%;":"width:90%;" ).'" onChange="updateSubTask(0,\''.$refType.'\','.$refId.');" value=""></div>';
       echo    '</td>';
       echo    '<td class="todoListTab" style="white-space:nowrap;text-align: center;">';
       echo      '<select dojoType="dijit.form.FilteringSelect" id="'.$refType.'_'.$refId.'_priorityNewSubTask_0" style="width:auto;"  class="input" readonly="true" >';
@@ -211,13 +245,18 @@ class SubTask extends SqlElement {
       echo    '</td>';
       echo  '</tr>';
     }
+    
     echo '</table>';
-    if (!$refresh) echo '</div></td></tr>';
-    if($dialogView) echo '</table>';
+    if(!$print){
+      if (!$refresh) echo '</div></td></tr>';
+      if($dialogView) echo '</table>';
+    }else{
+      echo '</td></tr>';
+    }
   }
   
   function drawStatusSubTask($id, $done, $idle, $handled,$refType,$refId,$gloablView,$rightUpdate,$rightRead){
-
+  
     $pos=1;
     $backgroundColor="";
     if($handled==1){
@@ -230,10 +269,11 @@ class SubTask extends SqlElement {
       $pos=4;
       $backgroundColor="background-color:#B7B3A9;";
     }
-    
-    
+  
+  
     echo '<div id="'.$refType.'_'.$refId.'_slidContainer_'.$id.'" >';
     echo '<table style="width:100%;height:100%;">';
+  
       echo '<tr>';
         echo ' <td style="width:12%;height:100%;">';
         echo '<div id="'.$refType.'_'.$refId.'_prev_'.$id.'" class="prev" style="'.(($id==0 or ($done==0 && $idle==0 && $handled==0) or ($rightUpdate=='NO' and $rightRead=='YES'))?'display:none':'').'" 
@@ -241,32 +281,32 @@ class SubTask extends SqlElement {
         echo ' </td>';
         echo ' <td style="width:76%;height:100%;'.$backgroundColor.'">';
           echo '<div  class="slideshow-container" style="width:100%;height:30px">';
-          echo '<input id="'.$refType.'_'.$refId.'_pos_'.$id.'" value="'.$pos.'" type="hidden" />';
           
-          echo '<div class="mySlides fade" style="'.(($done==0 && $idle==0 && $handled==0)?'display:block;':'display:none;').'margin-top:9px;padding-top:1px;">';
-          echo '  <div class="slideStatus">&nbsp;</div>';
+            echo '<input id="'.$refType.'_'.$refId.'_pos_'.$id.'" value="'.$pos.'" type="hidden" />';
+            
+            echo '<div class="mySlides fade" style="'.(($done==0 && $idle==0 && $handled==0)?'display:block;':'display:none;').'margin-top:9px;padding-top:1px;">';
+            echo '  <div class="slideStatus">&nbsp;</div>';
+            echo '</div>';
+            
+            echo '<div class="mySlides fade" style="'.(($handled==1)?'display:block;':'display:none;').'margin-top:9px;padding-top:1px;">';
+            echo '  <div class="slideStatus"><span  style="font-weight: bolder;">'.i18n('colHandled').'</span></div>';
+            echo '</div>';
+            
+            echo '<div class="mySlides fade" style="'.(($done==1)?'display:block;':'display:none;').'margin-top:9px;padding-top:1px;">';
+            echo '  <div class="slideStatus"><span  style="font-weight: bolder;">'.i18n('done').'</span></div>';
+            echo '</div>';
+            
+            echo '<div class="mySlides fade" style="'.(($idle==1)?'display:block;':'display:none;').'margin-top:9px;padding-top:1px;">';
+            echo   '<div class="slideStatus" ><span style="font-weight: bolder;">'.i18n('colIdle').'</span></div>';
+            echo '</div>';
           echo '</div>';
-          
-          echo '<div class="mySlides fade" style="'.(($handled==1)?'display:block;':'display:none;').'margin-top:9px;padding-top:1px;">';
-          echo '  <div class="slideStatus"><span  style="font-weight: bolder;">'.i18n('colHandled').'</span></div>';
-          echo '</div>';
-          
-          echo '<div class="mySlides fade" style="'.(($done==1)?'display:block;':'display:none;').'margin-top:9px;padding-top:1px;">';
-          echo '  <div class="slideStatus"><span  style="font-weight: bolder;">'.i18n('done').'</span></div>';
-          echo '</div>';
-          
-           echo '<div class="mySlides fade" style="'.(($idle==1)?'display:block;':'display:none;').'margin-top:9px;padding-top:1px;">';
-           echo   '<div class="slideStatus" ><span style="font-weight: bolder;">'.i18n('colIdle').'</span></div>';
-           echo '</div>';
-
-           echo '</div>';
-         echo ' </td>';
-         echo ' <td style="width:12%;height:100%;" >';
-         echo '<div  id="'.$refType.'_'.$refId.'_next_'.$id.'" class="next" style="'.(($id==0 or ($idle==1) or ($rightUpdate=="NO" and $rightRead=='YES'))?'display:none':'').'"  onclick="'.(($id!=0 and  $rightUpdate=='YES')?"nextSlides('next',".$id.",'".$refType."',".$refId.");":"").'">&#10095;</div>';
-         echo ' </td>';
-       echo '</tr>';
-     echo '</table>';
-     echo '</div>';
+        echo ' </td>';
+        echo ' <td style="width:12%;height:100%;" >';
+        echo '<div  id="'.$refType.'_'.$refId.'_next_'.$id.'" class="next" style="'.(($id==0 or ($idle==1) or ($rightUpdate=="NO" and $rightRead=='YES'))?'display:none':'').'"  onclick="'.(($id!=0 and  $rightUpdate=='YES')?"nextSlides('next',".$id.",'".$refType."',".$refId.");":"").'">&#10095;</div>';
+        echo ' </td>';
+      echo '</tr>';
+    echo '</table>';
+   echo '</div>';
   }
   
   
