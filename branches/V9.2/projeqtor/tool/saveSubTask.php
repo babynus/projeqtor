e8bb8738f9aeea2219ad81e2a95efa5922f24b62
florent <florent@f9421885-1815-481d-89b5-6d9585689fe2>
2021-04-20 08:20:50 +0000

    FAR

--- branches/V9.2/projeqtor/tool/saveSubTask.php
+++ branches/V9.2/projeqtor/tool/saveSubTask.php
@@ -55,8 +55,8 @@ if($element=='SubTask'){
     $subTask =($operation=='save')?new SubTask():new SubTask($idSubTask);
     if($operation=='update')$old=$subTask->getOld();
     if($name!=null)$subTask->name=$name;
-    $subTask->idPriority=intval($priority);
-    $subTask->idResource=intval($resource);
+    if($priority!=null)$subTask->idPriority=(intval($priority)!=0)?intval($priority):null;
+    if($priority!=null)$subTask->idResource=(intval($priority)!=0)?intval($resource):null;
     if($sortOrder!=null)$subTask->sortOrder=intval($sortOrder);
     if($operation=='update' and $status!=null){
       switch ($status){
