bd9989be448d86bdf05972e96171020ddddfe8eb
florent <florent.arnaud@projeqtor.net>
2021-06-17 10:39:44 +0000

    FAR

--- branches/V9.2/projeqtor/view/objectDetail.php
+++ branches/V9.2/projeqtor/view/objectDetail.php
@@ -8624,7 +8624,7 @@ function drawAffectationsFromObject($list, $obj, $type, $refresh=false) {
         if ($canDelete and !$print) {
           echo '  <a onClick="removeAffectation(\''.htmlEncode($aff->id).'\','.(($aff->idResource==getSessionUser()->id)?'1':'0').',\''.$classToShow.'\',\''.$idToShow.'\');" '.'title="'.i18n('removeAffectation').'" > '.formatSmallButton('Remove').'</a>';
         }
-        if ($canUpdate and !$print and !$aff->idle) {
+        if ($canUpdate and !$print and $isResource and !$aff->idle) {
           echo '  <a onClick="replaceAffectation('."'".htmlEncode($aff->id)."'".",'".get_class($obj)."'".",'".$type."'".",'".htmlEncode($aff->idResource)."'".",'".htmlEncode($aff->idProject)."'".",'".htmlEncode($aff->rate)."'".",'".htmlEncode($aff->idle)."'".",'".$aff->startDate."'".",'".htmlEncode($aff->endDate)."'".','.htmlEncode($aff->idProfile).');" '.'title="'.i18n('replaceAffectation').'" > '.formatSmallButton('SwitchUser').'</a>';
         } else {
           if ($aff->idle) {
@@ -8674,7 +8674,7 @@ function drawAffectationsFromObject($list, $obj, $type, $refresh=false) {
         if ($canDelete and !$print) {
           echo '  <a onClick="removeAffectation(\''.htmlEncode($aff->id).'\','.(($aff->idResource==getSessionUser()->id)?'1':'0').',\''.$classToShow.'\',\''.$idToShow.'\');" '.'title="'.i18n('removeAffectation').'" > '.formatSmallButton('Remove').'</a>';
         }
-        if ($canUpdate and !$print  and !$aff->idle) {
+        if ($canUpdate and !$print and $isResource and !$aff->idle) {
           echo '  <a onClick="replaceAffectation('."'".htmlEncode($aff->id)."'".",'".get_class($obj)."'".",'".$type."'".",'".htmlEncode($aff->idResource)."'".",'".htmlEncode($aff->idProject)."'".",'".htmlEncode($aff->rate)."'".",'".htmlEncode($aff->idle)."'".",'".$aff->startDate."'".",'".htmlEncode($aff->endDate)."'".','.htmlEncode($aff->idProfile).');" '.'title="'.i18n('replaceAffectation').'" > '.formatSmallButton('SwitchUser').'</a>';
         } else {
           if ($aff->idle) {
