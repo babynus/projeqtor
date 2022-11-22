2326bcefb086ebc26567a3d03049395813e70898
babynus <pascal.bernard@projeqtor.net>
2022-11-22 16:49:48 +0100

    Update index.php
    TEST COMMIT

--- projector/index.php
+++ projector/index.php
@@ -2,6 +2,7 @@
 /* ============================================================================
  * Default page. Redirects to view directory
  */
+// THIS IS A TEST
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" 
   "http://www.w3.org/TR/html4/strict.dtd">
