8c0e10564b5769ee3df3c7bbac06d736aa1936d9
Pascal BERNARD <pascal.bernard@projeqtor.net>
2022-11-29 18:05:46 +0100

    T-1 #comment OK

--- branches/V9.3/projeqtor/view/absenceMain.php
+++ branches/V9.3/projeqtor/view/absenceMain.php
@@ -23,7 +23,7 @@
  * about contributors at http://www.projeqtor.org 
  *     
  *** DO NOT REMOVE THIS NOTICE ************************************************/
-// TEST 12  
+// TEST 13
 /* ============================================================================
  * Presents an object. 
  */
