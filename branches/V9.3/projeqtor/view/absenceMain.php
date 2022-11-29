a9e0e812c0f7d93f17b66c4d17af78851fffee85
Pascal BERNARD <pascal.bernard@projeqtor.net>
2022-11-29 14:24:31 +0100

    TICKET-3 #comment validation 05

--- branches/V9.3/projeqtor/view/absenceMain.php
+++ branches/V9.3/projeqtor/view/absenceMain.php
@@ -23,7 +23,7 @@
  * about contributors at http://www.projeqtor.org 
  *     
  *** DO NOT REMOVE THIS NOTICE ************************************************/
-// TEST O4
+// TEST O5
 /* ============================================================================
  * Presents an object. 
  */
