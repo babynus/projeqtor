8594fda6e8ad6daf16214e6f389cec15048b4bbc
Pascal BERNARD <pascal.bernard@projeqtor.net>
2022-11-29 12:23:49 +0100

    TIKCET-3 #comment ok commenté #time 2h

--- branches/V9.3/projeqtor/view/absenceList.php
+++ branches/V9.3/projeqtor/view/absenceList.php
@@ -23,7 +23,7 @@
  * about contributors at http://www.projeqtor.org 
  *     
  *** DO NOT REMOVE THIS NOTICE ************************************************/
-// COMMIT TRY 
+// COMMIT TRY 02
 /* ============================================================================
  * Presents the list of objects of a given class.
  * TEST MODIF
